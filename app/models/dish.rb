947f91bff69199ed34676622f4feb22e95aea3ef
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-09-11 10:25:19 -0600

    Add ingredient relationship to Dish.

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,5 +1,5 @@
 class Dish <ApplicationRecord
   validates_presence_of :name, :description
   belongs_to :chef
-  has_many :ingredients
+  has_many :ingredients, through: :dish_ingredients
 end
--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -25,8 +25,8 @@ RSpec.describe 'dish show page', type: :feature do
     expect(page).to have_content(@dish_1.name)
     expect(page).to have_content(@dish_1.description)
     expect(page).to have_content(@dish_1.chef_id)
-    expect(page).to have_content(@ingretient_1)
+    expect(page).to have_content(@ingretient_1.name)
     expect(page).to_not have_content(@dish_2.chef_id)
-    expect(page).to_not have_content(@ingretient_3)
+    expect(page).to_not have_content(@ingretient_3.name)
   end
 end
