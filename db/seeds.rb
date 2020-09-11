fa069ab967045078bcd474de350c3ec75dfb3416
Daniel Halverson <daniel.halverson@hotmail.com>
2020-09-11 09:44:34 -0600

    US 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+chef_1 = Chef.create!(name: "Dan")
+chef_2 = Chef.create!(name: "Babish")
+dish_1 = chef_1.dishes.create!(name: "A Dish", description: "A Description")
+dish_2 = chef_1.dishes.create!(name: "B Dish", description: "B Description")
+dish_3 = chef_2.dishes.create!(name: "C Dish", description: "C Description")
+ingredient_1 = Ingredient.create!(name: "A Ingredient", calories: 100)
+ingredient_2 = Ingredient.create!(name: "B Ingredient", calories: 300)
+ingredient_3 = Ingredient.create!(name: "C Ingredient", calories: 400)
+ingredient_4 = Ingredient.create!(name: "D Ingredient", calories: 200)
+DishIngredient.create!(dish_id: dish_1.id, ingredient_id: ingredient_1.id)
+DishIngredient.create!(dish_id: dish_1.id, ingredient_id: ingredient_2.id)
---
+++ spec/features/dishes/show_spec.rb
@@ -0,0 +1,32 @@
+# Story 1 of 3
+# As a visitor
+# When I visit a dish's show page
+# I see the dish’s name and description
+# And I see a list of ingredients for that dish
+# And I see the chef's name
+require 'rails_helper'
+
+RSpec.describe "As a visitor, when I visit a dish's show page", type: :feature do
+  it "I see the dish name and description, list of ingredients, and the chefs name" do
+
+    chef_1 = Chef.create!(name: "Dan")
+    chef_2 = Chef.create!(name: "Babish")
+    dish_1 = chef_1.dishes.create!(name: "A Dish", description: "A Description")
+    dish_2 = chef_1.dishes.create!(name: "B Dish", description: "B Description")
+    dish_3 = chef_2.dishes.create!(name: "C Dish", description: "C Description")
+    ingredient_1 = Ingredient.create!(name: "A Ingredient", calories: 100)
+    ingredient_2 = Ingredient.create!(name: "B Ingredient", calories: 300)
+    ingredient_3 = Ingredient.create!(name: "C Ingredient", calories: 400)
+    ingredient_4 = Ingredient.create!(name: "D Ingredient", calories: 200)
+    DishIngredient.create!(dish_id: dish_1.id, ingredient_id: ingredient_1.id)
+    DishIngredient.create!(dish_id: dish_1.id, ingredient_id: ingredient_2.id)
+
+    visit("/dishes/#{dish_1.id}")
+    expect(page).to have_content(dish_1.name)
+    expect(page).to have_content(dish_1.description)
+    dish_1.ingredients.each do |ingred|
+      expect(page).to have_content(ingred.name)
+    end
+    expect(page).to have_content(dish_1.chef.name)
+  end
+end
