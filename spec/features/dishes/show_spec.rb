f96ccee1ff9f8740a62cd1a70168537fd8212da6
Arique <arique.aguilar@gmail.com>
2020-09-11 10:46:58 -0500

    Adds model tests and relationships for ingredients table

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe 'Dishes' do
   it 'can show the name, description, list of ingredients and the chefs name' do
     @chef = Chef.create!(name: 'Lasondra')
     @dish = @chef.dishes.create!(name: 'Meatballs', description: 'Beef in Marinera')
+    @ingredient_1 = @dish.ingredients.create!(name: "Beef", calories: 200)
+    @dish.ingredients.create!(name: 'Marinera', calories: 150)
 
     visit "dishes/#{@dish.id}"
     expect(page).to have_content("Meatballs")
