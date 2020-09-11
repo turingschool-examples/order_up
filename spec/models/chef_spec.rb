84057ce5f56c5c95a965890f6dfcbe4e913a3b6c
Travis McKinstry <mckinstrytg@gmail.com>
2020-09-11 10:27:27 -0600

    Create model test for 'most popular 3 ingredients'

--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -20,4 +20,19 @@ RSpec.describe Chef, type: :model do
       expect(george.all_ingredients).to eq(expected)
     end
   end
+
+  describe 'instance methods' do
+    it 'most popular ingredients' do
+      george = Chef.create!(name: "George")
+
+      dish_1 = Dish.create!(name: "Camel meatballs", description: "Succulent camel meat in spaghetti sauce", ingredients: "camel meat, spaghetti sauce, parmesean", calories: "3,455cal", chef_id: george.id)
+      dish_2 = Dish.create!(name: "Lasangua", description: "Layers of iguana lasangua", ingredients: "iguana, spaghetti sauce, parmesean, garlic", calories: "3,300cal", chef_id: george.id)
+      dish_3 = Dish.create!(name: "Chef Salad", description: "Fancy salad", ingredients: "iceberg lettuce, parmesean, garlic", calories: "300cal", chef_id: george.id)
+
+
+      expected = ["spaghetti sauce", "parmesean", "garlic"]
+
+      expect(george.most_popular).to eq(expected)
+    end
+  end
 end
