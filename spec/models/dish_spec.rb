f98bb1205fac65869d16675777501172a573d927
RyanDBarnett <conner2468@gmail.com>
2020-09-11 10:01:53 -0600

    Add dish model total calories spec test

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -1,12 +1,35 @@
 require 'rails_helper'
 
-RSpec.describe Dish, type: :model do
+RSpec.describe Dish do
   describe "validations" do
     it {should validate_presence_of :name}
     it {should validate_presence_of :description}
   end
+
   describe "relationships" do
     it {should belong_to :chef}
     it {should have_many :dish_ingredients}
+  end
+
+  describe 'instance methods' do
+    it 'total_calories' do
+      chef_ryan = Chef.create!(
+        name: 'Ryan'
+      )
+      @dish = chef_ryan.dishes.create!(
+        name: 'Test dish',
+        description: 'Test description'
+      )
+      @dish.ingredients.create!(
+        name: 'Pizza',
+        calories: 500
+      )
+      @dish.ingredients.create!(
+        name: 'Salad',
+        calories: 200
+      )
+
+      expect(@dish.total_calories).to eq(700)
+    end
   end
 end
