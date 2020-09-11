30e3806d1edf7c2f97c7a5f013034c9ac9f09a34
Travis McKinstry <mckinstrytg@gmail.com>
2020-09-11 10:16:50 -0600

    Create method to be able to collect all ingredients from all dishes from that one chef

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,5 +1,13 @@
-class Chef <ApplicationRecord
+class Chef < ApplicationRecord
   validates_presence_of :name
   has_many :dishes
+
+  def all_ingredients
+    array = []
+    ingreds = self.dishes.map{ |dish| dish.ingredients}
+    foods = ingreds.join(", ")
+    foods.split(", ").each{ |food| array << food}
+    array.uniq
+  end
 
 end
