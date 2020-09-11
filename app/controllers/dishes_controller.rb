1572d638f1c4975a668a0cf1cd226c8457933078
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-09-11 10:10:49 -0600

    update dishes controller

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -2,5 +2,13 @@ class DishesController < ApplicationController
 
   def show
     @dish = Dish.find(params[:id])
+    require "pry"; binding.pry
+  end
+
+  private
+
+  def dish_params
+    params.permit(:name,
+                  :dscription)
   end
 end
