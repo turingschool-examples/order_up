75a62ceccebf65314e27c6c5e84aaea8e6411c2a
Travis McKinstry <mckinstrytg@gmail.com>
2020-09-11 09:34:07 -0600

    Create test for user story 3

---
+++ spec/features/chef/show_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe "As a visitor" do
+  describe "When I visit a cehf's ingredient index page" do
+    describe "I see the name of that chef" do
+      describe "And I see a link to view a list of all ingredients that this chef uses in their dishes" do
+        describe "When I click on that link" do
+          it "I'm taken to a chef's ingredient index page and I can see a unique list of names of all the ingredients that this chef uses" do
+
+            george = Chef.create!(name: "George")
+
+            dish_1 = Dish.create!(name: "Camel meatballs", description: "Succulent camel meat in spaghetti sauce", ingredients: "camel meat, spaghetti sauce, parmesean", calories: "3,455cal", chef_id: george.id)
+            dish_2 = Dish.create!(name: "Lasangua", description: "Layers of iguana lasangua", ingredients: "iguana, spaghetti sauce, parmesean, garlic", calories: "3,300cal", chef_id: george.id)
+
+            visit "/chefs/#{george.id}"
+
+            expect(page).to have_content("#{george.name}")
+            expect(page).to have_link("See all of #{geroge.name}'s ingredients")
+            click_link("See all of #{geroge.name}'s ingredients")
+
+            expect(current_path).to eq("/chef/#{george.id}/ingredients")
+            expect(page).to have_content("#{dish_1.ingredients}")
+            expect(page).to have_content("#{dish_2.ingredients}")
+            expect(page).to_not have_content("spaghetti sauce").twice
+          end
+        end
+      end
+    end
+  end
+end
