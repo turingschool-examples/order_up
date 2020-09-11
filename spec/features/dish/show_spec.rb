83e7b0037e5be7e83afd4f76434888fa20ca142e
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-09-11 09:39:01 -0600

    Adds test for us2; us 2 complete

--- spec/features/dish/show_spec.rb
+++ spec/features/dish/show_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Dish show page", type: :feature do
   describe "As a visitor to dish show page" do
     before :each do
       @chef = Chef.create(name: "Mike Dao")
-      @dish = @chef.dishes.create(name: "Pizza", description: "Delicious", ingredients: "Pepperoni and Cheese")
+      @dish = @chef.dishes.create(name: "Pizza", description: "Delicious", ingredients: "Pepperoni and Cheese", calorie_count: 600)
     end
     it "I can see a the dish's name and description" do
 
@@ -25,5 +25,15 @@ RSpec.describe "Dish show page", type: :feature do
       visit "/dish/#{@dish.id}"
       expect(page).to have_content(@chef.name)
     end
+
+    it "I can see a total calorie count for that dish" do
+      visit "/dish/#{@dish.id}"
+
+      expect(page).to have_content(@dish.calorie_count)
+    end
   end
+#   Story 2 of 3
+# As a visitor
+# When I visit a dish's show page
+# I see the total calorie count for that dish.
 end
