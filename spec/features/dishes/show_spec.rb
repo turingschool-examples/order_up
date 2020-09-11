efc7df9ff5e1c94d6548ea7d9068880cfb029817
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-09-11 09:45:31 -0600

    Add more robust tests to Dishes show spec

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -4,12 +4,20 @@ RSpec.describe 'dish show page', type: :feature do
   before :each do
     @chef_1 = Chef.create(name: "Angelica")
     @chef_2 = Chef.create(name: "Big Freedia")
+    @chef_3 = Chef.create(name: "Kind Ron")
     @dish_1 = Dish.create(name: "Warm Bread",
                           description: "It's just, like, kinda warm bread",
-                          chef_id: @chef_1.id)
+                          chef_id: @chef_3.id)
     @dish_2 = Dish.create(name: "Hot and Spicy Chicken",
                           description: "Pretty good, very bouncy",
                           chef_id: @chef_2.id)
+    @ingretient_1 = Ingredient.create(name: "Bread",
+                                      calories: "200")
+    @ingretient_2 = Ingredient.create(name: "Chicken",
+                                      calories: "100")
+    @ingretient_3 = Ingredient.create(name: "Hot Peppers",
+                                      calories: "400")
+
     visit "/dishes/#{@dish_1.id}"
   end
 
@@ -17,8 +25,8 @@ RSpec.describe 'dish show page', type: :feature do
     expect(page).to have_content(@dish_1.name)
     expect(page).to have_content(@dish_1.description)
     expect(page).to have_content(@dish_1.chef_id)
+    expect(page).to have_content(@ingretient_1)
     expect(page).to_not have_content(@dish_2.chef_id)
-
-
+    expect(page).to_not have_content(@ingretient_3)
   end
 end
