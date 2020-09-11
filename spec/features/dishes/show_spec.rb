2ba431c2968b9d54dd912207580fcbb0a79e02bb
Chris Allbritton <cla.inbox@gmail.com>
2020-09-11 11:29:37 -0400

    Update show_spec to include all tested attributes

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -3,10 +3,13 @@ require 'rails_helper'
 RSpec.describe 'dish show page', type: :feature do
   it "has a dish name, description, ingredients, and the chef's name" do
     chef = Chef.create(name: "Chris")
-    dish = chef.dishes.create(name: "pizza", description: "tasty")
+    dish = chef.dishes.create(name: "pizza", description: "tasty", ingredients: "cheese and sauce")
 
     visit "/dishes/#{dish.id}"
 
     expect(page).to have_content(dish.name)
+    expect(page).to have_content(dish.description)
+    expect(page).to have_content(dish.ingredients)
+    expect(page).to have_content(dish.chef.name)
   end
 end
