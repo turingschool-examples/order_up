fbe1657f0a19af54aebf76e719af43477ba505d6
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-09-11 09:38:42 -0600

    Updated schema to include total calorie count for dishes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_11_152245) do
+ActiveRecord::Schema.define(version: 2020_09_11_153628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_09_11_152245) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "ingredients"
+    t.integer "calorie_count"
     t.index ["chef_id"], name: "index_dishes_on_chef_id"
   end
 
