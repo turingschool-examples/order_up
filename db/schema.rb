defc8fec685ec5c432785051d69312421efa19e9
Daniel Lessenden <dless27@gmail.com>
2020-09-11 10:31:12 -0500

    Add columns to dish table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_20_225836) do
+ActiveRecord::Schema.define(version: 2020_09_11_152541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,8 +27,12 @@ ActiveRecord::Schema.define(version: 2020_02_20_225836) do
     t.bigint "chef_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ingredients"
+    t.bigint "dish_id"
     t.index ["chef_id"], name: "index_dishes_on_chef_id"
+    t.index ["dish_id"], name: "index_dishes_on_dish_id"
   end
 
   add_foreign_key "dishes", "chefs"
+  add_foreign_key "dishes", "dishes"
 end
--- spec/features/dish/show_spec.rb
+++ spec/features/dish/show_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe 'dish show page' do
   describe 'As a visiter' do
     it 'has the dish name and desecription' do
-      pizza = Dish.create!(name: "pizza", description: "it is pizza...", ingredients: ["sauce", "cheese", "jalapenos"])
+      chef = Chef.create!(name: "Chef guy")
+      pizza = Dish.create!(name: "pizza", description: "it is pizza...", ingredients: ["sauce", "cheese", "jalapenos"], chef_id: chef.id)
 
       visit "/dish/#{pizza.id}"
       expect(page).to have_content(pizza.name)
