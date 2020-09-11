b9e22e91d7a39276ae02165c55bc6e217711ca71
Leah Riffell <leah.riffell@gmail.com>
2020-09-11 09:47:53 -0600

    Add test and method for getting list of ingredients for a dish

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -11,4 +11,21 @@ RSpec.describe Dish, type: :model do
     it {should have_many :dish_ingredients}
     it {should have_many(:ingredients).through(:dish_ingredients)}
   end
+
+  describe "instance methods" do 
+    before :each do 
+      @chef_1 = Chef.create!(name: "Dave Chang")
+
+      @dish_1 = @chef_1.dishes.create!(name: "Chawan Mushi", description: "steamed Japanese egg custard with mushrooms and crabmeat")
+  
+      @ingredient_1 = @dish_1.ingredients.create!(name: "Mushrooms", calories: "65")
+      @ingredient_2 = @dish_1.ingredients.create!(name: "Crabmeat", calories: "82")
+    end
+
+    describe ".ingredient_list" do
+      it "can list of ingredients for a dish" do
+        expect(@dish_1.ingredient_list).to eq([@ingredient_1.name, @ingredient_2.name])
+      end
+    end
+  end
 end
