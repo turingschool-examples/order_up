b6cd4b7819db7177ba9f73ec5af0999bbaf0fd94
Hashim Gari <hashmaster3k@gmail.com>
2020-09-11 10:59:26 -0600

    Working AR query for US 4

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,4 +1,9 @@
 class Chef <ApplicationRecord
   validates_presence_of :name
   has_many :dishes
+
+  def top_3_ingredients
+    require "pry"; binding.pry
+    #Ingredient.joins(:dishes).group(:name).count
+  end
 end
--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -12,16 +12,28 @@ RSpec.describe 'CHEFS SHOW PAGE' do
     @dish_2 = @chef.dishes.create!(name: 'Grilled Chicken', description: 'Everything is better grilled, even chicken.')
     @ing_3 = @dish_2.ingredients.create!(name: 'chicken', calories: 200)
     @ing_4 = @dish_2.ingredients.create!(name: 'grill spices', calories: 5)
-
-    visit "/chefs/#{@chef.id}"
   end
 
   it 'displays chefs name and has link to list all ingredients that chef uses in dishes' do
+    visit "/chefs/#{@chef.id}"
     expect(page).to have_content(@chef.name)
     expect(page).to have_link("List of all ingredients used")
   end
 
   it 'can goto the chefs ingredients index page' do
+    visit "/chefs/#{@chef.id}"
     click_link "List of all ingredients used"
+  end
+
+  it 'displays three most popular ingredients based on how many dishes that ingredient is in' do
+    dish_3 = @chef.dishes.create!(name: 'Meat Kebab', description: 'The best darn kebabs in the city')
+    dish_3.ingredients.create!(name: 'lamb', calories: 215)
+    dish_3.ingredients << @ing_2 #spice wezeal
+    dish_3.ingredients << @ing_3 #chicken
+    dish_3.ingredients << @ing_4 #grilling spices
+
+    visit "/chefs/#{@chef.id}"
+    expect(page).to have_content("Three most popular ingredients: ")
+    expect(page).to have_content(@ing_2.name, @ing_3.name, @ing_4.name)
   end
 end
