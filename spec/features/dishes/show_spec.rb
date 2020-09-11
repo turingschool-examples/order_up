1984ffd9234cffdf2d13cbb683fe674d750bdaa7
RyanDBarnett <conner2468@gmail.com>
2020-09-11 09:26:19 -0600

    Add before each in dish show spec and chef name test

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -1,18 +1,24 @@
 require 'rails_helper'
 
 describe "As a visitor when I visit a dish's show page" do
-  it "I see the dish’s name and description" do
+  before :each do
     chef_ryan = Chef.create!(
       name: 'Ryan'
     )
-    dish = chef_ryan.dishes.create!(
+    @dish = chef_ryan.dishes.create!(
       name: 'Test dish',
       description: 'Test description'
     )
 
-    visit "/dishes/#{dish.id}"
+    visit "/dishes/#{@dish.id}"
+  end
 
-    expect(page).to have_content(dish.name)
-    expect(page).to have_content(dish.description)
+  it "I see the dish’s name and description" do
+    expect(page).to have_content(@dish.name)
+    expect(page).to have_content(@dish.description)
+  end
+
+  it "I see the chef's name" do
+    expect(page).to have_content(@dish.chef.name)
   end
 end
