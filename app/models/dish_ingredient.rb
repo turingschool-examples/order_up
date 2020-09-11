fce3f7bb5e392b65fcd397a3a6b4ebf8877e00ce
Dani Coleman <danicoleman00@gmail.com>
2020-09-11 10:06:00 -0600

    Fix relationships

--- app/models/dish_ingredient.rb
+++ app/models/dish_ingredient.rb
@@ -1,5 +1,4 @@
 class DishIngredient <ApplicationRecord
-  # validates_presence_of :name, :calories
   belongs_to :dish
   belongs_to :ingredient
 end
--- db/schema.rb
+++ db/schema.rb
@@ -23,9 +23,9 @@ ActiveRecord::Schema.define(version: 2020_09_11_150929) do
 
   create_table "dish_ingredients", force: :cascade do |t|
     t.bigint "dish_id"
-    t.bigint "ingredients_id"
+    t.bigint "ingredient_id"
     t.index ["dish_id"], name: "index_dish_ingredients_on_dish_id"
-    t.index ["ingredients_id"], name: "index_dish_ingredients_on_ingredients_id"
+    t.index ["ingredient_id"], name: "index_dish_ingredients_on_ingredient_id"
   end
 
   create_table "dishes", force: :cascade do |t|
@@ -43,6 +43,6 @@ ActiveRecord::Schema.define(version: 2020_09_11_150929) do
   end
 
   add_foreign_key "dish_ingredients", "dishes"
-  add_foreign_key "dish_ingredients", "ingredients", column: "ingredients_id"
+  add_foreign_key "dish_ingredients", "ingredients"
   add_foreign_key "dishes", "chefs"
 end
