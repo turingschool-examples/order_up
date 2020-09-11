5fa6e00f60efaae471a4a488295596b4d8f49d4f
RyanDBarnett <conner2468@gmail.com>
2020-09-11 10:03:01 -0600

    Add calory count test to dish show spec

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -22,17 +22,21 @@ describe "As a visitor when I visit a dish's show page" do
   end
 
   it "I see the dish’s name and description" do
-    expect(page).to have_content(@dish.name)
-    expect(page).to have_content(@dish.description)
+    expect(page).to have_content('Test dish')
+    expect(page).to have_content('Test description')
   end
 
   it "I see the chef's name" do
-    expect(page).to have_content(@dish.chef.name)
+    expect(page).to have_content('Ryan')
   end
 
   it 'I see a list of ingredients for that dish' do
     @dish.ingredients.each do |ingredient|
       expect(page).to have_content(ingredient.name)
     end
+  end
+
+  it 'I see the total calorie count for that dish' do
+    expect(page).to have_content('Total Calories: 700')
   end
 end
