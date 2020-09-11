2838e5e2e9fc6ce1d124d064cf44271378f19349
Daniel Halverson <daniel.halverson@hotmail.com>
2020-09-11 10:54:35 -0600

    Final commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
 
   get "/dishes/:dish_id", to: "dishes#show"
+
+  get "/chefs/:chef_id", to: "chefs#show"
+
+  get "/chefs/:chef_id/ingredients", to: "chefs#index"
+
 end
