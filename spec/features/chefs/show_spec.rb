36eeb0ab9b3874ebd1b3cea6ecc147b0e8d22bac
RyanDBarnett <conner2468@gmail.com>
2020-09-11 10:45:06 -0600

    Add link to view all chef ingredients to chef show spec

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 describe "As a visitor when I visit a chef's show page" do
   before :each do
-    chef_ryan = Chef.create!(
+    @chef_ryan = Chef.create!(
       name: 'Ryan'
     )
-    @dish = chef_ryan.dishes.create!(
+    @dish = @chef_ryan.dishes.create!(
       name: 'Test dish',
       description: 'Test description'
     )
@@ -18,10 +18,18 @@ describe "As a visitor when I visit a chef's show page" do
       calories: 200
     )
 
-    visit "/chefs/#{chef_ryan.id}"
+    visit "/chefs/#{@chef_ryan.id}"
   end
 
   it 'I see the name of that chef' do
     expect(page).to have_content('Ryan')
+  end
+
+  it 'I see a link to view a list of all ingredients' do
+    expect(page).to have_link('Ingredients This Chef Uses')
+    
+    click_link 'Ingredients This Chef Uses'
+
+    expect(current_path).to eq("/chefs/#{@chef_ryan.id}/ingredients")
   end
 end
