7e7910e0717fadf30c091a362375acb8355caec0
Philip DeFraties <philipjfry338@gmail.coom>
2020-09-11 10:10:30 -0600

    Add ingredient and dish_ingredient model tests, models, tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+@chef = Chef.create!(name: 'Phil')
+
+@dish = @chef.dishes.create!(name: 'Grilled Cheese', description: 'Gooey cheese bread')
+
+@ingredient_1 = Ingredient.create!(name: "Slice Smoked Cheddar", calories: 150)
+@ingredient_2 = Ingredient.create!(name: "2 Slices Sourdough Bread", calories: 200)
+@ingredient_3 = Ingredient.create!(name: "Tbs Butter", calories: 100)
+
+@dish_ingredient_1 = DishIngredient.create!(dish: @dish, ingredient: @ingredient_1)
+@dish_ingredient_2 = DishIngredient.create!(dish: @dish, ingredient: @ingredient_2)
+@dish_ingredient_3 = DishIngredient.create!(dish: @dish, ingredient: @ingredient_3)
