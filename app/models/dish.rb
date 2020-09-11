c64dbb1c2fe161a44eb74811c38123c135ca8d1c
Priya Power <priyajag72@gmail.com>
2020-09-11 10:48:47 -0600

    Add method calorie_count to Dish Model - not fully AR

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -4,4 +4,10 @@ class Dish <ApplicationRecord
   has_many :ingredients, through: :dish_ingredients
 
   validates_presence_of :name, :description
+
+  def calorie_count
+    # attempting AR call.... lots of errors... I need to work on many to many AR calls
+    # I really want to use .sum - I am having trouble accessing the information I want using AR calls/queries/aggregate methods
+    ingredients.select(:calories).pluck(:calories).sum(&:to_f)
+  end
 end
