19e0fa35ce3c11b546805bbe0b3b137491bbd566
Chris Allbritton <cla.inbox@gmail.com>
2020-09-11 11:49:51 -0400

    Update tables to suit the tests and update test to account for dish.ingredients

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_11_152455) do
+ActiveRecord::Schema.define(version: 2020_09_11_154650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,5 +31,22 @@ ActiveRecord::Schema.define(version: 2020_09_11_152455) do
     t.index ["chef_id"], name: "index_dishes_on_chef_id"
   end
 
+  create_table "dishes_ingredients", force: :cascade do |t|
+    t.bigint "dishes_id"
+    t.bigint "ingredients_id"
+    t.index ["dishes_id"], name: "index_dishes_ingredients_on_dishes_id"
+    t.index ["ingredients_id"], name: "index_dishes_ingredients_on_ingredients_id"
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.integer "calories"
+    t.bigint "dish_id"
+    t.index ["dish_id"], name: "index_ingredients_on_dish_id"
+  end
+
   add_foreign_key "dishes", "chefs"
+  add_foreign_key "dishes_ingredients", "dishes", column: "dishes_id"
+  add_foreign_key "dishes_ingredients", "ingredients", column: "ingredients_id"
+  add_foreign_key "ingredients", "dishes"
 end
--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.describe 'dish show page', type: :feature do
   it "has a dish name, description, ingredients, and the chef's name" do
     chef = Chef.create(name: "Chris")
-    dish = chef.dishes.create(name: "pizza", description: "tasty", ingredients: "cheese and sauce")
-
+    dish = chef.dishes.create(name: "pizza", description: "tasty")
+    ingredient = dish.ingredients.create(name: "sauce", calories: 240)
     visit "/dishes/#{dish.id}"
 
     expect(page).to have_content(dish.name)
     expect(page).to have_content(dish.description)
-    expect(page).to have_content(dish.ingredients)
+    expect(page).to have_content(dish.ingredients.name)
     expect(page).to have_content(dish.chef.name)
   end
 end
