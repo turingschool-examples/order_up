c8c39be3ae5657b4bf10c5b7c032736b7a4481f3
Travis McKinstry <mckinstrytg@gmail.com>
2020-09-11 10:46:02 -0600

    Create method within model to calcualte most popular ingredient

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -10,4 +10,15 @@ class Chef < ApplicationRecord
     array.uniq
   end
 
+  def most_popular
+    array = []
+    ingreds = self.dishes.map{ |dish| dish.ingredients}
+    foods = ingreds.join(", ")
+    all_ingreds = foods.split(", ").each{ |food| array << food}
+    number= all_ingreds.each_with_object(Hash.new(0)) { |ingred,counts| counts[ingred] += 1 }
+    sorted_num =  number.sort_by{ |k,v| v}.reverse
+    only_foods = sorted_num.map{ |element| element[0]}
+    only_foods[0..2]
+  end
+
 end
