6d3a6afc4113bd88daf9775292f29701dde1e7c0
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-09-11 09:27:17 -0600

    Updated schema with ingredients column in dishes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_20_225836) do
+ActiveRecord::Schema.define(version: 2020_09_11_152245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_02_20_225836) do
     t.bigint "chef_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ingredients"
     t.index ["chef_id"], name: "index_dishes_on_chef_id"
   end
 
