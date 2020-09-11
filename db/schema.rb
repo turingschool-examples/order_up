14a73011ab0a3ae04d8c4813bafe86784ada2936
Travis McKinstry <mckinstrytg@gmail.com>
2020-09-11 09:24:41 -0600

    Add 'calories' to the table 'dishes'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_11_151516) do
+ActiveRecord::Schema.define(version: 2020_09_11_152239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_09_11_151516) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "ingredients"
+    t.string "calories"
     t.index ["chef_id"], name: "index_dishes_on_chef_id"
   end
 
