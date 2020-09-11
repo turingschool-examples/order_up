67e870a02445cc3405fe4970d960265e0d176417
Leah Riffell <leah.riffell@gmail.com>
2020-09-11 09:21:29 -0600

    Add models and table relationships

--- spec/models/dish_ingredient_spec.rb
+++ spec/models/dish_ingredient_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Dish, type: :model do
+RSpec.describe DishIngredient, type: :model do
   describe "relationships" do
     it {should belong_to :dish}
     it {should belong_to :ingredient}
