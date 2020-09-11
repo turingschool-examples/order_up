c5c229c3f16e5d645f1a1254f6c97a9564d855c8
Travis McKinstry <mckinstrytg@gmail.com>
2020-09-11 09:11:34 -0600

    Create test for user story 1

---
+++ spec/features/visitor/show_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe "As a visitor" do
+  describe "When I visit a dish's show page" do
+    describe "I see the dish's name and description" do
+      describe "And I see a list of ingredients for that dish" do
+        it "And I see the chef's name" do
+
+          george = Chef.create!(name: "George")
+
+          dish_1 = Dish.create!(name: "Camel meatballs", description: "Succulent camel meat in spaghetti sauce", chef_id: george.id)
+          visit "/dishes/#{dish_1.id}"
+
+          expect(page).to have_content("#{dish_1.name}")
+          expect(page).to have_content("#{dish_1.description}")
+          expect(page).to have_content("#{dish_1.ingredients}")
+          expect(page).to have_content("#{george.name}")
+        end
+      end
+    end
+  end
+end
