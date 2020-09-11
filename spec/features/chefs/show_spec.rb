86b9057b8d2b3af8b5eb349347fc27b957d7b0e8
RyanDBarnett <conner2468@gmail.com>
2020-09-11 10:52:54 -0600

    Add popular ingredients test to chef show spec

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -9,6 +9,10 @@ describe "As a visitor when I visit a chef's show page" do
       name: 'Test dish',
       description: 'Test description'
     )
+    @dish_2 = @chef_ryan.dishes.create!(
+      name: 'Test dish',
+      description: 'Test description'
+    )
     @dish.ingredients.create!(
       name: 'Pizza',
       calories: 500
@@ -16,6 +20,31 @@ describe "As a visitor when I visit a chef's show page" do
     @dish.ingredients.create!(
       name: 'Salad',
       calories: 200
+    )
+    @dish.ingredients.create!(
+      name: 'Kale',
+      calories: 100
+    )
+    @dish.ingredients.create!(
+      name: 'Ice Cream',
+      calories: 400
+    )
+    @dish.ingredients.create!(
+      name: 'Sugar',
+      calories: 300
+    )
+
+    @dish_2.ingredients.create!(
+      name: 'Pizza',
+      calories: 500
+    )
+    @dish_2.ingredients.create!(
+      name: 'Ice Cream',
+      calories: 400
+    )
+    @dish_2.ingredients.create!(
+      name: 'Sugar',
+      calories: 300
     )
 
     visit "/chefs/#{@chef_ryan.id}"
@@ -27,9 +56,17 @@ describe "As a visitor when I visit a chef's show page" do
 
   it 'I see a link to view a list of all ingredients' do
     expect(page).to have_link('Ingredients This Chef Uses')
-    
+
     click_link 'Ingredients This Chef Uses'
 
     expect(current_path).to eq("/chefs/#{@chef_ryan.id}/ingredients")
+  end
+
+  it 'I see the three most popular ingredients that the chef uses in their dishes' do
+    within('.popular-ingredients') do
+      expect(page).to have_content('Pizza')
+      expect(page).to have_content('Ice Cream')
+      expect(page).to have_content('Sugar')
+    end
   end
 end
