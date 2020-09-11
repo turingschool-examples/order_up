0d53015e8e5a65aa208a9be7205745a53a8b59ff
Alex Desjardins <ardesjardins91@gmail.com>
2020-09-11 09:40:42 -0600

    Set-up complete. DB tables created.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+#Chefs
+wolfgang = Chef.create(name: "Wolfgang Puck")
+ramsey = Chef.create(name: "Gordon Ramsey")
+
+#Dishes
+ribeye = wolfgang.dishes.create(name: "Classic Ribeye Steak with Potatoes", description: "Locally farmed and very tender")
+lamb = wolfgang.dishes.create(name: "Rosemary Lamb Shank and Asparagus", description: "Best lamb ever")
+
+#Ingredients
+ribeye = Ingredient.create(name: "Ribeye", calories: 350)
+potatoes = Ingredient.create(name: "Potatoe", calories: 125)
+
+lamb = Ingredient.create(name: "Lamb", calories: 200)
+rosemary = Ingredient.create(name: "Rosemary", calories: 25)
+asparagus = Ingredient.create(name: "Asparagus", calories: 100)
