5be7762cd117efbb0f16dee7f6a9644de62d362a
fieldstyler <tdfields93@gmail.com>
2020-09-11 10:42:15 -0600

    Complete User Story 3

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -14,14 +14,8 @@ RSpec.describe Dish, type: :model do
   it 'should count calories' do
     ty = Chef.create(name: "Tyler Fields")
     pizza = ty.dishes.create(name: "Pizza", description: "Yum!")
-    bng = ty.dishes.create(name: "Biscuits and Gravy", description: "Yum!")
-    cbb = ty.dishes.create(name: "Chicken Burrito Bowl", description: "Yum!")
     pep = pizza.ingredients.create(name: "Pepperoni", calories: 200)
     dough = pizza.ingredients.create(name: "Pizza Dough", calories: 300)
-    flour = bng.ingredients.create(name: "Flour", calories: 100)
-    saus = bng.ingredients.create(name: "Sausage", calories: 500)
-    chik = cbb.ingredients.create(name: "Chicken", calories: 150)
-    @dishes = Dish.all
 
     expect(pizza.calorie_count).to eq(500)
   end
