5f669ce338226c8dc283085027142fe45067cd8b
Leah Riffell <leah.riffell@gmail.com>
2020-09-11 09:33:13 -0600

    Create seed data to use in tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Dish.destroy_all
+Ingredient.destroy_all
+Chef.destroy_all
+
+chef_1 = Chef.create(name: "Dave Chang")
+
+dish_1 = chef_1.dishes.create(name: "Chawan Mushi", description: "steamed Japanese egg custard with mushrooms and crabmeat")
+
+ingredient_1 = dish_1.ingredients.create(name: "Mushrooms", calories: "65")
+ingredient_2 = dish_1.ingredients.create(name: "Crabmeat", calories: "82")
+
+
