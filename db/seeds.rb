647cdbc2b4cfd8e003054f268ca0fab1162f088c
Logan Riffell <lkriffell@gmail.com>
2020-09-11 10:57:49 -0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+chef = Chef.create!(name: "Daniel")
+dish = Dish.create!(name: "fruit bowl", description: "A bowl of fruit", chef_id: "#{chef.id}")
+dish2 = Dish.create!(name: "Orangaid", description: "pureed oranges", chef_id: "#{chef.id}")
+ingredient = Ingredient.create!(name: "Apple", calories: 50)
+ingredient2 = Ingredient.create!(name: "Orange", calories: 70)
+DishIngredient.create!(dish_id: "#{dish.id}", ingredient_id: "#{ingredient.id}")
+DishIngredient.create!(dish_id: "#{dish.id}", ingredient_id: "#{ingredient2.id}")
+DishIngredient.create!(dish_id: "#{dish2.id}", ingredient_id: "#{ingredient2.id}")
