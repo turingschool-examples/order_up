dd4a1eaa745cd9913126b9df6758b9b79e8c71cc
Nick King <nickmaxking@gmail.com>
2020-10-29 10:11:30 -0600

    Add total_calories helper method

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -4,4 +4,8 @@ class Dish < ApplicationRecord
   belongs_to :chef
   has_many :recipes
   has_many :ingredients, through: :recipes
+
+  def total_calories
+    ingredients.sum(:calories)
+  end
 end
