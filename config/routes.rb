f93c636768568ab1ab8c05bb39356c7a8d1f3511
Zach Stearns <zach.stearns92@gmail.com>
2020-10-29 10:11:00 -0600

    Create chefs namespace

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   get '/dishes/:dish_id', to: 'dishes#show'
 
   get '/chefs/:chef_id', to: 'chefs#show'
+
+  namespace :chefs do
+    get '/:chef_id/ingredients', to: 'ingredients#index'
+  end
 end
