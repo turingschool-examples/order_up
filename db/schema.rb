ab836351cc38ec47504e77503889fc25bf85cb66
Jake Heft <jakeheft@gmail.com>
2020-10-29 09:21:19 -0600

    Change incorrectly named column that affected the foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_150653) do
+ActiveRecord::Schema.define(version: 2020_10_29_151811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,9 +23,9 @@ ActiveRecord::Schema.define(version: 2020_10_29_150653) do
 
   create_table "dish_ingredients", force: :cascade do |t|
     t.bigint "dish_id"
-    t.bigint "ingredients_id"
+    t.bigint "ingredient_id"
     t.index ["dish_id"], name: "index_dish_ingredients_on_dish_id"
-    t.index ["ingredients_id"], name: "index_dish_ingredients_on_ingredients_id"
+    t.index ["ingredient_id"], name: "index_dish_ingredients_on_ingredient_id"
   end
 
   create_table "dishes", force: :cascade do |t|
@@ -43,6 +43,6 @@ ActiveRecord::Schema.define(version: 2020_10_29_150653) do
   end
 
   add_foreign_key "dish_ingredients", "dishes"
-  add_foreign_key "dish_ingredients", "ingredients", column: "ingredients_id"
+  add_foreign_key "dish_ingredients", "ingredients"
   add_foreign_key "dishes", "chefs"
 end
