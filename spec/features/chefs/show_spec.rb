5232fb3f70d67a9961c2696a4d4ccb44fd6ce9d0
Will Dunlap <dunlapww@gmail.com>
2020-10-29 10:10:22 -0600

    Add feature test for chef show page: name and link

---
+++ spec/features/chefs/show_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+# As a visitor
+# When I visit a chef's show page
+# I see the name of that chef
+# And I see a link to view a list of all ingredients that this chef uses in their dishes
+# When I click on that link
+# I'm taken to a chef's ingredient index page
+# and I can see a unique list of names of all the ingredients that this chef uses
+
+describe 'As a visitor' do
+  describe "When I visit a chef's show page" do
+    before :each do
+      @chef1 = Chef.create({name: "Ralph"})
+      @dishr = @chef1.dishes.create({name: "pasta w red sauce", description: "noodles and red sauce"})
+      @dishw = @chef1.dishes.create({name: "pasta w white sauce", description: "noodles and white sauce"})
+      @noodles = Ingredient.create({name: "noodles", calories: 100})
+      @wsauce = Ingredient.create({name: "white sauce", calories: 80})
+      @rsauce = Ingredient.create({name: "red sauce", calories: 75})
+      @dishr_noodles = DishIngredient.create({dish_id: @dishr.id, ingredient_id: @noodles.id})
+      @dishr_rsauce = DishIngredient.create({dish_id: @dishr.id, ingredient_id: @rsauce.id})
+      @dishw_noodles = DishIngredient.create({dish_id: @dishw.id, ingredient_id: @noodles.id})
+      @dishw_wsauce = DishIngredient.create({dish_id: @dishw.id, ingredient_id: @wsauce.id})
+    end
+    it "I see the chef's name and a link to view all his ingredients" do
+      visit "/chefs/#{@chef1.id}"
+      expect(page.to have_content("Chef Name: #{@chef1.name}"))
+      expect(page.to have_link("Chef's Ingredients"))
+    end
+  end
+end
