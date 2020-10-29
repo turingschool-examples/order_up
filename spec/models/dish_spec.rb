139cbcac3b1cd66e8ca3daf2489abe534535218c
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-10-29 09:46:58 -0600

    Create method for total_calories

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -5,29 +5,33 @@ RSpec.describe Dish, type: :model do
     it {should validate_presence_of :name}
     it {should validate_presence_of :description}
   end
+
   describe "relationships" do
     it {should belong_to :chef}
     it {should have_many(:dish_ingredients)}
     it {should have_many(:ingredients).through(:dish_ingredients)}
   end
-  describe "instance methods" do
-    @chef    = Chef.create!(name: "Gordon Ramsay")
-    @dish    = Dish.create!(name: "Avocado and Egg Toast",
-                            description: "Smashed avocado toast with hardboiled eggs.",
-                            chef_id: @chef.id)
-    @avocado = Ingredient.create!(name: "Avocado",
-                                  calories: 120)
-    @toast   = Ingredient.create!(name: "Toasted Multigrain Bread",
-                                  calories: 180)
-    @egg     = Ingredient.create!(name: "Hardboiled Egg",
-                                  calories: 90)
-@dish_ing_1  = DishIngredient.create!(dish_id: @dish.id,
-                                      ingredient_id: @avocado.id)
-@dish_ing_2  = DishIngredient.create!(dish_id: @dish.id,
-                                      ingredient_id: @toast.id)
-@dish_ing_3  = DishIngredient.create!(dish_id: @dish.id,
-                                      ingredient_id: @egg.id)
 
-  expect(@dish.total_calories).to eq(390)
+  describe "instance methods" do
+    it 'can count total calories of dish' do
+          @chef    = Chef.create!(name: "Gordon Ramsay")
+          @dish    = Dish.create!(name: "Avocado and Egg Toast",
+                              description: "Smashed avocado toast with hardboiled eggs.",
+                              chef_id: @chef.id)
+          @avocado = Ingredient.create!(name: "Avocado",
+                                    calories: 120)
+          @toast   = Ingredient.create!(name: "Toasted Multigrain Bread",
+                                    calories: 180)
+          @egg     = Ingredient.create!(name: "Hardboiled Egg",
+                                    calories: 90)
+      @dish_ing_1  = DishIngredient.create!(dish_id: @dish.id,
+                                        ingredient_id: @avocado.id)
+      @dish_ing_2  = DishIngredient.create!(dish_id: @dish.id,
+                                        ingredient_id: @toast.id)
+      @dish_ing_3  = DishIngredient.create!(dish_id: @dish.id,
+                                        ingredient_id: @egg.id)
+
+    expect(@dish.total_calories).to eq(390)
+    end
   end
 end
