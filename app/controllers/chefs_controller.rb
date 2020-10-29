4d41dcd36664104e04848322c9469e58b3a4c9aa
James Belta <james.belta@gmail.com>
2020-10-29 10:55:36 -0600

    ingredient action w/chef

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -3,4 +3,9 @@ class ChefsController < ApplicationController
   def show
     @chef = Chef.find(params[:id])
   end
+
+  def ingredients
+    @chef = Chef.find(params[:id])
+    binding.pry
+  end
 end
