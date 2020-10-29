f63dc71a7a2e43797a3c271382eec533ff50f289
John Kim <abcdefghijohnk@gmail.com>
2020-10-29 11:03:07 -0600

    create model test for total caloreis method

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -8,4 +8,19 @@ RSpec.describe Dish, type: :model do
   describe "relationships" do
     it {should belong_to :chef}
   end
+
+  it 'total_calories' do
+    tanaka = Chef.create!(name: "Chef Tanaka")
+    sushi = tanaka.dishes.create!(name: "Sushi",
+                                  description: "Awesome",
+                                 )
+    salmon = Ingredient.create!(name: "Salmon", calories: 50)
+    rice = Ingredient.create!(name: "Rice", calories: 80)
+    wasabi = Ingredient.create!(name: "Wasabi", calories: 10)
+    DishIngredient.create!(dish: sushi, ingredient: salmon)
+    DishIngredient.create!(dish: sushi, ingredient: rice)
+    DishIngredient.create!(dish: sushi, ingredient: wasabi)
+
+    expect(sushi.total_calories).to eq(140)
+  end
 end
