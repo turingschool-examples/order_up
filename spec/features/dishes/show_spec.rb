d73e21542be34554771ed0ae36016c9593354e45
Jose Lopez <joselopez11394@gmail.com>
2020-10-29 11:56:25 -0400

    adds test for total calories in dishes show page and for calorie_count method in dish model

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -1,15 +1,8 @@
 require "rails_helper"
 
-# Story 1 of 3
-# As a visitor
-# When I visit a dish's show page
-# I see the dish’s name and description
-# And I see a list of ingredients for that dish
-# And I see the chef's name
-
 describe "As a visitor" do
   describe "When I visit a dish's show page" do
-    it "I see the dish’s name and description " do
+    it "I see the dish’s name and description." do
       bob = Chef.create!(name: "Bob")
       pasta = Dish.create!(name: "Pasta", description: "Noodle Dish", chef_id: bob.id)
       noodles = Ingredient.create!(name: "Noodles", calories: 50)
@@ -28,6 +21,22 @@ describe "As a visitor" do
       expect(page).to have_content(sause.name)
       expect(page).to have_content(meatballs.name)
       expect(page).to have_content(bob.name)
+    end
+
+    it "I see the total calorie count for that dish." do
+      bob = Chef.create!(name: "Bob")
+      pasta = Dish.create!(name: "Pasta", description: "Noodle Dish", chef_id: bob.id)
+      noodles = Ingredient.create!(name: "Noodles", calories: 50)
+      sause = Ingredient.create!(name: "Sause", calories: 100)
+      meatballs = Ingredient.create!(name: "Meatballs", calories: 150)
+
+      DishIngredient.create!(dish_id: pasta.id, ingredient_id: noodles.id)
+      DishIngredient.create!(dish_id: pasta.id, ingredient_id: sause.id)
+      DishIngredient.create!(dish_id: pasta.id, ingredient_id: meatballs.id)
+
+      visit "/dishes/#{pasta.id}"
+
+      expect(page).to have_content(300)
     end
   end
 end
--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -10,4 +10,20 @@ RSpec.describe Dish, type: :model do
     it {should have_many :dish_ingredients}
     it {should have_many(:ingredients).through(:dish_ingredients)}
   end
+
+  describe "Instance Methods" do
+    it "Calorie Count Method" do
+      bob = Chef.create!(name: "Bob")
+      pasta = Dish.create!(name: "Pasta", description: "Noodle Dish", chef_id: bob.id)
+      noodles = Ingredient.create!(name: "Noodles", calories: 50)
+      sause = Ingredient.create!(name: "Sause", calories: 100)
+      meatballs = Ingredient.create!(name: "Meatballs", calories: 150)
+
+      DishIngredient.create!(dish_id: pasta.id, ingredient_id: noodles.id)
+      DishIngredient.create!(dish_id: pasta.id, ingredient_id: sause.id)
+      DishIngredient.create!(dish_id: pasta.id, ingredient_id: meatballs.id)
+
+      expect(Dish.calorie_count(pasta)).to eq(300)
+    end
+  end
 end
