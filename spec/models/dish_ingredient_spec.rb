87021a0c4bb772322c62d973bf3104de4c635644
Aidan Murray <aidanhansm@gmail.com>
2020-10-29 10:39:49 -0600

    Add spec for dish total calories

--- spec/models/dish_ingredient_spec.rb
+++ spec/models/dish_ingredient_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe DishIngredient, type: :model do
   describe 'relationships' do
     it do
-      should have_one :dish
-      should have_one :ingredient
+      should belong_to :dish
+      should belong_to :ingredient
     end
   end
 end
--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -1,11 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Dish, type: :model do
-  describe "validations" do
-    it {should validate_presence_of :name}
-    it {should validate_presence_of :description}
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :description }
   end
-  describe "relationships" do
-    it {should belong_to :chef}
+  describe 'relationships' do
+    it { should belong_to :chef }
+    it { should have_many :dish_ingredients }
+    it { should have_many(:ingredients).through(:dish_ingredients) }
+  end
+
+  describe 'instance methods' do
+    before :each do
+      @chef1 = Chef.create!(name: 'gerald')
+
+      @dish1 = Dish.create!(name: 'cheese',
+                            description: 'its cheese',
+                            chef_id: @chef1.id)
+
+      @ingredient1 = Ingredient.create!(name: 'milk',
+                                        calories: 4)
+
+      @ingredient2 = Ingredient.create!(name: 'other cheese stuff',
+                                        calories: 6)
+
+      @ingredient3 = Ingredient.create!(name: 'more cheese stuff',
+                                        calories: 8)
+
+      @dish1.ingredients << @ingredient1
+      @dish1.ingredients << @ingredient2
+      @dish1.ingredients << @ingredient3
+    end
+    it '#total_calories' do
+      expect(@dish1.total_calories).to eq(18)
+    end
   end
 end
