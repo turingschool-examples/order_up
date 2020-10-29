ec656be26daca5b8e73f64ea23b870db716394bb
Curtis Bartell <cbartell980@gmail.com>
2020-10-29 09:37:39 -0600

    Add model relationship tests

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 describe Ingredient, type: :model do
   describe 'relationships' do
-    it { should have_many(:dish_ingredients }
+    it { should have_many :dish_ingredients }
   end
 end
