13b0b1de649f484cdb7f93050f350afdef50a6fd
Nick King <nickmaxking@gmail.com>
2020-10-29 09:46:20 -0600

    Add test for total calorie count display

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -42,4 +42,41 @@ describe 'When I visit a dish show page as a visitor' do
     expect(page).to have_content(sauce.name)
     expect(page).to have_content(julia.name)
   end
+
+  it 'I see the total calorie count for that dish' do
+    julia = Chef.create!(name: "Julia Child")
+    chicken_parm = Dish.new(
+      name: "Chicken Parmesan",
+      description: "Cheesy breaded chicken",
+      chef: julia
+    )
+    chicken = Ingredient.create!(
+      name: "Chicken",
+      calories: "200"
+    )
+    mozzerella = Ingredient.create!(
+      name: "Mozzerella",
+      calories: "150"
+    )
+    sauce = Ingredient.create!(
+      name: "Tomato Sauce",
+      calories: "90"
+    )
+    Recipe.create!(
+      dish: chicken_parm,
+      ingredient: chicken
+    )
+    Recipe.create!(
+      dish: chicken_parm,
+      ingredient: mozzerella
+    )
+    Recipe.create!(
+      dish: chicken_parm,
+      ingredient: sauce
+    )
+
+    visit "/dishes/#{chicken_parm.id}"
+
+    expect(page).to have_content("Total Calories: 440")
+  end
 end
