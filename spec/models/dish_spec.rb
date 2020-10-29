d2c6d8ee3a40fc5cfa8a7db6240068502042c5b8
Jake Heft <jakeheft@gmail.com>
2020-10-29 09:52:35 -0600

    Add model test for total calories of dish

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -10,4 +10,40 @@ RSpec.describe Dish, type: :model do
     it { should have_many :dish_ingredients }
     it { should have_many(:ingredients).through(:dish_ingredients) }
   end
+  describe "instance methods" do
+    it "#total_calories" do
+      chef = Chef.create!(name: "Bob Newhart")
+      dish = Dish.create!(
+        name: "Spaghetti",
+        description: "Spaghetti and Meatballs",
+        chef_id: chef.id
+      )
+      pasta = Ingredient.create!(
+        name: "Pasta",
+        calories: 150
+      )
+      sauce = Ingredient.create!(
+        name: "Marinara Sauce",
+        calories: 75
+      )
+      meatballs = Ingredient.create!(
+        name: "Meatballs",
+        calories: 300
+      )
+      DishIngredient.create!(
+        dish_id: dish.id,
+        ingredient_id: pasta.id
+      )
+      DishIngredient.create!(
+        dish_id: dish.id,
+        ingredient_id: sauce.id
+      )
+      DishIngredient.create!(
+        dish_id: dish.id,
+        ingredient_id: meatballs.id
+      )
+
+      expect(dish.total_calories).to eq(525)
+    end
+  end
 end
