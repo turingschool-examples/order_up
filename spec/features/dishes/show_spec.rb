d083deb5afdb080bcbcf9e702777b166ced9d84e
Roberto Basulto <roberto.basulto085@gmail.com>
2020-10-29 11:59:31 -0400

    Created test to see total calories

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -33,5 +33,22 @@ describe "As a visitor" do
       expect(page).to have_content(tomato.name)
       expect(page).to have_content(onion.name)
     end
+
+    it "I see the total calories for the dish" do
+      gordon = Chef.create({name: "Gordon Ramsey"})
+      burger = Dish.create({name: "Cheese Burger", description: "Famous Burger", chef_id: gordon.id})
+      buns = Ingredient.create({name: "Buns", calories: 60})
+      patty = Ingredient.create({name: 'Patty', calories: 120})
+      tomato = Ingredient.create({name: 'Tomato', calories: 30})
+      onion = Ingredient.create({name: 'Onion', calories: 20})
+      DishIngredient.create({dish_id: burger.id, ingredient_id: buns.id})
+      DishIngredient.create({dish_id: burger.id, ingredient_id: patty.id})
+      DishIngredient.create({dish_id: burger.id, ingredient_id: tomato.id})
+      DishIngredient.create({dish_id: burger.id, ingredient_id: onion.id})
+
+      visit "/dishes/#{burger.id}"
+
+      expect(page).to have_content("Total dish calories: #{burger.total_calories}")
+    end
   end
 end
