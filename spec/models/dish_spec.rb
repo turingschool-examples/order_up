378c203535dbac94f3106aea02f90b3ffea8b238
Hope Gochnour <hope.gochnour@gmail.com>
2020-10-29 10:04:13 -0600

    add model test for dish#total_calories method

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -10,4 +10,13 @@ RSpec.describe Dish, type: :model do
     it { should have_many :recipes }
     it { should have_many(:ingredients).through(:recipes)}
   end
+  describe "methods" do
+    chef = Chef.create(name: "Gordon Ramsay")
+    spaghetti = chef.dishes.create(name: "Spaghetti", description: "A light, classical pasta dish with succulent tomato sauce and zesty home-made meatballs")
+    pasta = spaghetti.ingredients.create(name: "Pasta", calories: 1200)
+    basil = spaghetti.ingredients.create(name: "Basil", calories: 20)
+    meatball = spaghetti.ingredients.create(name: "Meatball", calories: 600)
+    tomato = spaghetti.ingredients.create(name: "Tomato", calories: 60)
+    expect(dish.total_calories).to eq(1880)
+  end
 end
