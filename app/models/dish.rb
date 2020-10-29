2d4589c478390c580a13fa9c27fca99d7a45dfa0
Will Dunlap <dunlapww@gmail.com>
2020-10-29 09:58:16 -0600

    Add dish model method calories

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -3,4 +3,11 @@ class Dish <ApplicationRecord
   belongs_to :chef
   has_many :dish_ingredients
   has_many :ingredients, through: :dish_ingredients
+
+  def calories
+    ingredients.sum(:calories)
+  end
+
+
+
 end
