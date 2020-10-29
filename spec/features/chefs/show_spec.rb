0c7081b58d7e6e12758a192e232572b5a969545d
Zach Stearns <zach.stearns92@gmail.com>
2020-10-29 10:53:52 -0600

    WIP, trying to figure out extension

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -5,6 +5,7 @@ describe "As a visitor" do
     before(:each) do
       @chef = Chef.create!({name: 'Matty Meatballs'})
     end
+
     it "I see the name of that chef and a link to view a list of all
         ingredients this chef uses in their dishes" do
       visit "/chefs/#{@chef.id}"
@@ -18,6 +19,110 @@ describe "As a visitor" do
 
       click_link "This Chef's Favorite Ingredients"
       expect(current_path).to eq("/chefs/#{@chef.id}/ingredients")
+    end
+
+    it "I see the 3 most popular ingredients that chef uses" do
+      dish_1 = Dish.create!({
+        name: 'Meatballs',
+        description: 'Hearty, comes with four.',
+        chef_id: "#{@chef.id}"
+        })
+
+      dish_2 = Dish.create!({
+        name: 'Meatball sub',
+        description: 'Big, delicious, filling.',
+        chef_id: "#{@chef.id}"
+        })
+
+      dish_3 = Dish.create!({
+        name: 'Meatball Mac n Cheese',
+        description: 'Gooey and warm.',
+        chef_id: "#{@chef.id}"
+        })
+
+      dish_4 = Dish.create!({
+        name: "Plain Mac",
+        description: 'For the simpletons',
+        chef_id: "#{@chef.id}"
+        })
+
+      ingredient_1 = Ingredient.create!({
+        name: 'Meatballs',
+        calories: 360
+        })
+
+      ingredient_2 = Ingredient.create!({
+        name: 'Marinara sauce',
+        calories: 30
+        })
+
+      ingredient_4 = Ingredient.create!({
+        name: 'Bun',
+        calories: 70
+        })
+
+      ingredient_5 = Ingredient.create!({
+        name: 'Cheese',
+        calories: 60
+        })
+
+      ingredient_6 = Ingredient.create!({
+        name: 'Mac',
+        calories: 60
+        })
+
+      d_i_meatball_1 = DishIngredient.create!({
+        dish_id: dish_1.id,
+        ingredient_id: ingredient_1.id
+        })
+
+      d_i_meatball_2 = DishIngredient.create!({
+        dish_id: dish_2.id,
+        ingredient_id: ingredient_1.id
+        })
+
+      d_i_meatball_2 = DishIngredient.create!({
+        dish_id: dish_3.id,
+        ingredient_id: ingredient_1.id
+        })
+
+      d_i_marinara = DishIngredient.create!({
+        dish_id: dish_1.id,
+        ingredient_id: ingredient_2.id
+        })
+
+
+      d_i_bun = DishIngredient.create!({
+        dish_id: dish_2.id,
+        ingredient_id: ingredient_4.id
+        })
+
+      d_i_cheese_1 = DishIngredient.create!({
+        dish_id: dish_2.id,
+        ingredient_id: ingredient_5.id
+        })
+
+      d_i_cheese_2 = DishIngredient.create!({
+        dish_id: dish_3.id,
+        ingredient_id: ingredient_5.id
+        })
+
+      d_i_mac = DishIngredient.create!({
+        dish_id: dish_3.id,
+        ingredient_id: ingredient_6.id
+        })
+
+      d_i_mac_2 = DishIngredient.create!({
+        dish_id: dish_4.id,
+        ingredient_id: ingredient_6.id
+        })
+
+      visit "/chefs/#{@chef.id}"
+
+      expect(page).to have_content("Top Ingredients")
+      expect(page).to have_content("Meatballs")
+      expect(page).to have_content("Cheese")
+      expect(page).to have_content("Mac")
     end
   end
 end
--- spec/models/dish_ingredient_spec.rb
+++ spec/models/dish_ingredient_spec.rb
@@ -1,6 +1,111 @@
 require 'rails_helper'
 
 describe DishIngredient, type: :model do
-  it { should belong_to :dish }
-  it { should belong_to :ingredient }
+  describe "relationships" do
+    it { should belong_to :dish }
+    it { should belong_to :ingredient }
+  end
+
+  it "should calculate how often ingredient used" do
+    @chef = Chef.create!({
+      name: 'Matty Meatballs'
+      })
+
+    dish_1 = Dish.create!({
+      name: 'Meatballs',
+      description: 'Hearty, comes with four.',
+      chef_id: "#{@chef.id}"
+      })
+
+    dish_2 = Dish.create!({
+      name: 'Meatball sub',
+      description: 'Big, delicious, filling.',
+      chef_id: "#{@chef.id}"
+      })
+
+    dish_3 = Dish.create!({
+      name: 'Meatball Mac n Cheese',
+      description: 'Gooey and warm.',
+      chef_id: "#{@chef.id}"
+      })
+
+    dish_4 = Dish.create!({
+      name: "Plain Mac",
+      description: 'For the simpletons',
+      chef_id: "#{@chef.id}"
+      })
+
+    ingredient_1 = Ingredient.create!({
+      name: 'Meatballs',
+      calories: 360
+      })
+
+    ingredient_2 = Ingredient.create!({
+      name: 'Marinara sauce',
+      calories: 30
+      })
+
+    ingredient_4 = Ingredient.create!({
+      name: 'Bun',
+      calories: 70
+      })
+
+    ingredient_5 = Ingredient.create!({
+      name: 'Cheese',
+      calories: 60
+      })
+
+    ingredient_6 = Ingredient.create!({
+      name: 'Mac',
+      calories: 60
+      })
+
+    d_i_meatball_1 = DishIngredient.create!({
+      dish_id: dish_1.id,
+      ingredient_id: ingredient_1.id
+      })
+
+    d_i_meatball_2 = DishIngredient.create!({
+      dish_id: dish_2.id,
+      ingredient_id: ingredient_1.id
+      })
+
+    d_i_meatball_2 = DishIngredient.create!({
+      dish_id: dish_3.id,
+      ingredient_id: ingredient_1.id
+      })
+
+    d_i_marinara = DishIngredient.create!({
+      dish_id: dish_1.id,
+      ingredient_id: ingredient_2.id
+      })
+
+
+    d_i_bun = DishIngredient.create!({
+      dish_id: dish_2.id,
+      ingredient_id: ingredient_4.id
+      })
+
+    d_i_cheese_1 = DishIngredient.create!({
+      dish_id: dish_2.id,
+      ingredient_id: ingredient_5.id
+      })
+
+    d_i_cheese_2 = DishIngredient.create!({
+      dish_id: dish_3.id,
+      ingredient_id: ingredient_5.id
+      })
+
+    d_i_mac = DishIngredient.create!({
+      dish_id: dish_3.id,
+      ingredient_id: ingredient_6.id
+      })
+
+    d_i_mac_2 = DishIngredient.create!({
+      dish_id: dish_4.id,
+      ingredient_id: ingredient_6.id
+      })
+
+require "pry"; binding.pry
+  end
 end
