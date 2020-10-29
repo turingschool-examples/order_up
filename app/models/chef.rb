9b0e125fb902ba64c88b59a2c500b334e9623d06
Shaun James <ShaunDaneJames@Gmail.com>
2020-10-29 10:40:28 -0600

    Creates model tests for find ingredients method

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -2,6 +2,8 @@ class Chef < ApplicationRecord
   validates_presence_of :name
   has_many :dishes
 
+
+#Too much ruby, find an AR solution
   def find_ingredients
     dishes.flat_map do |dish|
       dish.ingredients.flat_map do |ingredient|
--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -7,4 +7,28 @@ RSpec.describe Chef, type: :model do
   describe "relationships" do
     it {should have_many :dishes}
   end
+
+  describe "instance methods" do
+    it "find_ingredients" do
+      chef_steve = Chef.create!(name: "Steve")
+      pbj = chef_steve.dishes.create!(name: "PeanutButter Jelly",
+                         description: "Most delicious")
+      peanutbutter = pbj.ingredients.create!(name: "PeanutButter",
+                                             calories: 1337)
+      jelly = pbj.ingredients.create!(name: "Jelly",
+                                             calories: 9999)
+      bread = pbj.ingredients.create!(name: "Bread",
+                                             calories: 250)
+      ham_sammy = chef_steve.dishes.create!(name: "Ham Sammy",
+                         description: "Most delicious")
+      ham = ham_sammy.ingredients.create!(name: "Ham",
+                                             calories: 1337)
+      cheese = ham_sammy.ingredients.create!(name: "Cheese",
+                                             calories: 9999)
+      bread = ham_sammy.ingredients.create!(name: "Bread",
+                                             calories: 250)
+
+      expect(chef_steve.find_ingredients).to eq(["PeanutButter", "Jelly", "Bread", "Ham", "Cheese"])                                       
+    end
+  end
 end
