f37ad417ba8d457d34b865e5a67a116c1548d778
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-29 10:15:39 -0600

    Add test for all ingredients method

--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -7,4 +7,21 @@ RSpec.describe Chef, type: :model do
   describe "relationships" do
     it {should have_many :dishes}
   end
+
+  describe "instance methods" do
+    before :each do
+      @chef = Chef.create!(name: "Chef Casey")
+      @dish = @chef.dishes.create!(name: "Mac n Cheese", description: "Creammy goodness")
+      @cheese = Ingredient.create!(name: "Cheese", calories: 750)
+      @pasta = Ingredient.create!(name: "Pasta", calories: 300)
+      @breadcrumbs = Ingredient.create!(name: "Breadcrumbs", calories: 175)
+      DishIngredient.create!(dish_id: @dish.id, ingredient_id: @cheese.id)
+      DishIngredient.create!(dish_id: @dish.id, ingredient_id: @pasta.id)
+      DishIngredient.create!(dish_id: @dish.id, ingredient_id: @breadcrumbs.id)
+    end
+
+    it "#all_chef_ingredients" do
+      expect(@chef.all_chef_ingredients).to eq (["Cheese", "Pasta", "Breadcrumbs"])
+    end
+  end
 end
