7a008c1c7e1ebbb425816ea46d83bfbd79447eea
Nick King <nickmaxking@gmail.com>
2020-10-29 10:41:31 -0600

    Add test for most used ingredients display

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -81,4 +81,96 @@ describe 'As a visitor, when I visit a chef show page' do
     expect(page).to have_content("Bread", count: 1)
     expect(page).to have_content("Maple Syrup", count: 1)
   end
+
+  it "I see the chef's three most used ingredients" do
+    julia = Chef.create!(name: "Julia Child")
+    chicken_parm = Dish.new(
+      name: "Chicken Parmesan",
+      description: "Cheesy breaded chicken",
+      chef: julia
+    )
+    chicken = Ingredient.create!(
+      name: "Chicken",
+      calories: "200"
+    )
+    mozzerella = Ingredient.create!(
+      name: "Mozzerella",
+      calories: "150"
+    )
+    sauce = Ingredient.create!(
+      name: "Tomato Sauce",
+      calories: "90"
+    )
+    egg = Ingredient.create!(
+      name: "Egg",
+      calories: 78
+    )
+    Recipe.create!(
+      dish: chicken_parm,
+      ingredient: chicken
+    )
+    Recipe.create!(
+      dish: chicken_parm,
+      ingredient: mozzerella
+    )
+    Recipe.create!(
+      dish: chicken_parm,
+      ingredient: sauce
+    )
+    Recipe.create!(
+      dish: chicken_parm,
+      ingredient: egg
+    )
+    french_toast = Dish.new(
+      name: "French Toast",
+      description: "Custardy bread with syrup",
+      chef: julia
+    )
+    bread = Ingredient.create!(
+      name: "Bread",
+      calories: 120
+    )
+    syrup = Ingredient.create!(
+      name: "Maple Syrup",
+      calories: 150
+    )
+    Recipe.create!(
+      dish: french_toast,
+      ingredient: egg
+    )
+    Recipe.create!(
+      dish: french_toast,
+      ingredient: bread
+    )
+    Recipe.create!(
+      dish: french_toast,
+      ingredient: syrup
+    )
+    maple_scramble = Dish.new(
+      name: "Scrambled Maple Cheese Eggs",
+      description: "Pretty awful actually",
+      chef: julia
+    )
+    Recipe.create!(
+      dish: maple_scramble,
+      ingredient: egg
+    )
+    Recipe.create!(
+      dish: maple_scramble,
+      ingredient: mozzerella
+    )
+    Recipe.create!(
+      dish: maple_scramble,
+      ingredient: syrup
+    )
+    visit "/chefs/#{julia.id}"
+
+    expect(page).to have_content("Julia Child's Most Used Ingredients:")
+
+    within '#most-used' do
+      expect(page).to have_content("Egg")
+      expect(page).to have_content("Maple Syrup")
+      expect(page).to have_content("Mozzerella Cheese")
+    end
+  end
 end
