ed6bc4682d0fcf6cbe17f185896e2b15a8c7b31d
Zach Stearns <zach.stearns92@gmail.com>
2020-10-29 10:11:26 -0600

    Add test for link to chef's favorite ingredients

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -2,14 +2,22 @@ require 'rails_helper'
 
 describe "As a visitor" do
   describe "When I visit a chef's show page" do
+    before(:each) do
+      @chef = Chef.create!({name: 'Matty Meatballs'})
+    end
     it "I see the name of that chef and a link to view a list of all
         ingredients this chef uses in their dishes" do
-        @chef = Chef.create!({name: 'Matty Meatballs'})
-
       visit "/chefs/#{@chef.id}"
 
       expect(page).to have_content("#{@chef.name}")
       expect(page).to have_link("This Chef's Favorite Ingredients")
+    end
+
+    it "When I click that link I am taken to the chef's ingredient index page" do
+      visit "/chefs/#{@chef.id}"
+
+      click_link "This Chef's Favorite Ingredients"
+      expect(current_path).to eq("/chefs/#{@chef.id}/ingredients")
     end
   end
 end
