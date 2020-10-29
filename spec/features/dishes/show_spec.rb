7abefbdc41eb4a16f2b3524196c02190d7e1b0e2
Connor Ferguson <cpfergus1@gmail.com>
2020-10-29 13:17:21 -0300

    implemented user story 2 test

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -6,8 +6,10 @@ describe 'As a visitor' do
     @dish = Dish.create!(name: 'cornbread',
                         description: 'bread',
                         chef_id: @chef.id)
-    @ingredient1 = Ingredient.create!(name: 'corn')
-    @ingredient2 = Ingredient.create!(name: 'butter')
+    @ingredient1 = Ingredient.create!(name: 'corn',
+                                      calories: 100)
+    @ingredient2 = Ingredient.create!(name: 'butter',
+                                      calories: 500)
     DishesIngredient.create!(dish: @dish, ingredient: @ingredient1)
     DishesIngredient.create!(dish: @dish, ingredient: @ingredient2)
   end
@@ -25,6 +27,13 @@ describe 'As a visitor' do
     it "And I see the chef's name" do
       visit "/dishes/#{@dish.id}"
       expect(page).to have_content(@chef.name)
+    end
+  end
+  #Story 2 of 3
+  descirbe "When I visit a dish's show page" do
+    it "I see the total calorie count for that dish." do
+        visit "/dishes/#{@dish.id}"
+        expect(page).to have_content(@dish.calorie_count)
     end
   end
 end
