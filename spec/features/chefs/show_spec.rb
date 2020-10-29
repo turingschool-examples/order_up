88c05319fd0c132a35708d5a56b33ce74dd731df
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-29 10:53:37 -0600

    Begin to write test for 3 most popular items

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -15,6 +15,14 @@ describe "As a visitor" do
     @dough = Ingredient.create!(name: "Dough", calories: 300)
     DishIngredient.create!(dish_id: @dish_2.id, ingredient_id: @cheese.id)
     DishIngredient.create!(dish_id: @dish_2.id, ingredient_id: @dough.id)
+    @dish_3 = @chef.dishes.create!(name: "Calzone", description: "Folded Pizza")
+    DishIngredient.create!(dish_id: @dish_3.id, ingredient_id: @cheese.id)
+    DishIngredient.create!(dish_id: @dish_3.id, ingredient_id: @dough.id)
+    @dish_4 = @chef.dishes.create!(name: "Pasta Special", description: "Daily Special")
+    @pasta = Ingredient.create!(name: "Pasta", calories: 300)
+    @butter = Ingredient.create!(name: "Butter", calories: 500)
+    DishIngredient.create!(dish_id: @dish_4.id, ingredient_id: @pasta.id)
+    DishIngredient.create!(dish_id: @dish_4.id, ingredient_id: @butter.id)
   end
 
   describe "When I visit a chef's show page" do
@@ -38,6 +46,20 @@ describe "As a visitor" do
       expect(page).to have_content(@pasta.name)
       expect(page).to have_content(@breadcrumbs.name)
       expect(page).to have_content(@dough.name)
+    end
+  end
+
+  describe "When I visit a chef's show page" do
+    it "I see the three most popular ingredients that the chef uses in their dishes (Popularity is based off of how many dishes use that ingredient)" do
+      visit "/chefs/#{@chef.id}"
+
+      expect(page).to have_content("3 Most Popular Ingredients")
+
+      within "#popular" do
+        expect(page).to have_content(@cheese.name)
+        expect(page).to have_content(@dough.name)
+        expect(page).to have_content(@pasta.name)
+      end
     end
   end
 end
