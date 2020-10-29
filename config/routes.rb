72aa727b1a29cccce9dd98f9f73efd22b2351ade
Eduardo Parra <helloeduardo@icloud.com>
2020-10-29 10:22:46 -0600

    Add routes using resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/dishes/:id', to: 'dishes#show'
+  resources :dishes, only: [:show]
+
+  resources :chefs, only: [:show] do
+    resources :ingredients, only: [:index]
+  end
 end
