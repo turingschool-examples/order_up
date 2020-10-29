65683952c41af939e33b1b1fe9545aa1bef94303
Roberto Basulto <roberto.basulto085@gmail.com>
2020-10-29 12:57:58 -0400

    Created specs for ingredient in show page and spec for model method

--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -7,4 +7,32 @@ RSpec.describe Chef, type: :model do
   describe "relationships" do
     it {should have_many :dishes}
   end
+
+  describe 'model methods' do
+    it "should return a uniq list of ingredients the chef uses" do
+      gordon = Chef.create({name: "Gordon Ramsey"})
+
+      burger = Dish.create({name: "Cheese Burger", description: "Famous Burger", chef_id: gordon.id})
+      buns = Ingredient.create({name: "Buns", calories: 60})
+      patty = Ingredient.create({name: 'Patty', calories: 120})
+      tomato = Ingredient.create({name: 'Tomato', calories: 30})
+      onion = Ingredient.create({name: 'Onion', calories: 20})
+      DishIngredient.create({dish_id: burger.id, ingredient_id: buns.id})
+      DishIngredient.create({dish_id: burger.id, ingredient_id: patty.id})
+      DishIngredient.create({dish_id: burger.id, ingredient_id: tomato.id})
+      DishIngredient.create({dish_id: burger.id, ingredient_id: onion.id})
+
+      chicken_alfredo = Dish.create({name: "Fettuccine Alfredo", description: "Famous pasta", chef_id: gordon.id})
+      pasta = Ingredient.create({name: "Pasta", calories: 80})
+      chicken = Ingredient.create({name: "Chicken", calories: 150})
+      DishIngredient.create({dish_id: chicken_alfredo.id, ingredient_id: pasta.id})
+      DishIngredient.create({dish_id: chicken_alfredo.id, ingredient_id: chicken.id})
+
+      cheese = Ingredient.create({name: "Cheese", calories: 60})
+      DishIngredient.create({dish_id: chicken_alfredo.id, ingredient_id: cheese.id})
+      DishIngredient.create({dish_id: burger.id, ingredient_id: cheese.id})
+
+      expect(gordon.all_ingredients).to eq([buns,patty,tomato,onion,pasta,chicken,cheese])
+    end
+  end
 end
