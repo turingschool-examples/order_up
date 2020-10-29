d0ba99b3e84d5fb6927927c6511503a70608afe3
Austin Aspaas <araspaas@live.com>
2020-10-29 10:47:00 -0600

    Add chef show page routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   resources :dishes, only: :show
+  resources :chefs, only: :show
+  get "/chefs/:chef_id/ingredients", to: "chefs#ingredients_index", as: "chef_ingredients"
 end
