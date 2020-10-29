d749538cf3ed52e02a1347c70ccbe448615ef3d7
John Kim <abcdefghijohnk@gmail.com>
2020-10-29 10:01:06 -0600

    create dish ingredients model

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,4 +1,7 @@
 class Dish <ApplicationRecord
+  has_many :dish_ingredients
+  has_many :ingredients, through: :dish_ingredients
+  
   validates_presence_of :name, :description
   belongs_to :chef
 end
