56567464366d1ac5b71e28feed209e6ea6c7ebfd
Will Dunlap <dunlapww@gmail.com>
2020-10-29 10:37:58 -0600

    Add method for chef ingredients

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -2,4 +2,8 @@ class Chef <ApplicationRecord
   validates_presence_of :name
   has_many :dishes
 
+  def ingredients
+    dishes.joins(:ingredients).select("ingredients.name").distinct.pluck("ingredients.name")
+  end
+
 end
