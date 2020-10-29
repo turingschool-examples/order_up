18f6cc52023c89c86779b874836c99ec3c3b4473
George Soderholm <georgesoderholm@gmail.com>
2020-10-29 09:38:21 -0600

    Adds feature test for dish show page: to see dish name, and description; a list of ingredients and the chef's name who created the masterpeice

---
+++ spec/features/dish/show_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'Dish Show Page' do
+  describe 'As a visitor' do
+    before :each do
+      @murray = Chef.create!(name: 'Murray McMasterful')
+      @spaghetti = @murray.dishes.create!(name: 'Spaghetti', description: 'Delish!')
+      @pasta = @spaghetti.ingredients.create!(name: 'Rigitoni', calories: 10)
+      @sauce = @spaghetti.ingredients.create!(name: 'Red Sauce', calories: 50)
+      @salt = @spaghetti.ingredients.create!(name: 'Salt', calories: 5)
+    end
+    it 'can see the name and description of the dish' do
+      visit "/dishes/#{@spaghetti.id}"
+
+      expect(page).to have_content("Name: #{@spaghetti.name}")
+      expect(page).to have_content("Description: #{@spaghetti.description}")
+
+      expect(page).to have_content("#{@spaghetti.name}'s ingredients: ")
+        @spaghetti.ingredient.each do |ingredient|
+          within "#ingredient-#{@spaghetti.id}" do
+            expect(page).to have_content(ingredient.name)
+          end
+        end
+      expect(page).to have_content("Created by: #{@spaghetti.chef.name}")
+    end
+  end
+end
