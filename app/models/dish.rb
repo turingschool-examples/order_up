d98d74eba3ddbe5c57c056862e257aa8adc86ddf
Carson Jardine <carsonjardine@gmail.com>
2020-10-29 09:49:39 -0600

    Update code to pass US2

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,6 +1,13 @@
 class Dish <ApplicationRecord
   validates_presence_of :name, :description
+
   belongs_to :chef
+
   has_many :dish_ingredients
   has_many :ingredients, through: :dish_ingredients
+
+  def total_calories
+    ingredients.sum(:calories)
+  end
+
 end
