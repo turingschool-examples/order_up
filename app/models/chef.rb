b6526ad084bfe655831845f9c9e9281c4fc0da94
Will Dunlap <dunlapww@gmail.com>
2020-10-29 10:59:40 -0600

    remove pry

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -3,12 +3,11 @@ class Chef <ApplicationRecord
   has_many :dishes
 
   def ingredients
-    require 'pry'; binding.pry
     dishes.joins(:ingredients).select("ingredients.name").distinct
   end
 
   #insufficient time to test method
-  
+
   # def top_three_ingredients
   #   dishes.joins(:ingredients).select("count(ingredients.name) as dish_count, ingredients.name").group("ingredients.name").order(dish_count: :desc).limit(3)
   # end
