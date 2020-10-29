e80ff3d0cd76a85db293db80b3c3feec1f9e87d0
Joshua Carey <jdcarey128@gmail.com>
2020-10-29 10:39:49 -0600

    Fix distinct ingredients test to account for order

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -3,7 +3,12 @@ class Chef <ApplicationRecord
   has_many :dishes
 
   def distinct_ingredients
-    self.dishes.joins(:ingredients).pluck('ingredients.name').uniq
+    self.dishes.joins(:ingredients).pluck('ingredients.name').uniq.sort
+  end
+
+  def popular_ingredients
+    require "pry"; binding.pry
+    Ingredient.joins(:dishes).where('dishes.chef_id = self').select('ingredients.name, count(*)').group(:id)
   end
 
 end
--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -36,9 +36,44 @@ RSpec.describe Chef, type: :model do
       DishIngredient.create!(dish_id: spaghetti.id, ingredient_id: wine.id)
       DishIngredient.create!(dish_id: spaghetti.id, ingredient_id: basil.id)
 
-      ingredients = [egg.name, tomato.name, zucc.name, basil.name, sausage.name, wine.name]
+      ingredients = [egg.name, tomato.name, zucc.name, basil.name, sausage.name, wine.name].sort
 
       expect(franc.distinct_ingredients).to eq(ingredients)
+    end
+
+    it 'popular_ingredients' do
+      franc = Chef.create!(name: "Francois Francesco")
+      ratatouille = franc.dishes.create!(
+                                          name: "Ratatouille",
+                                          description: "Spicy and delicious!"
+                                        )
+      spaghetti = franc.dishes.create!(
+                                          name: "Spaghetti and Meatballs",
+                                          description: "Mouthwatering and flavourful."
+                                        )
+      egg = Ingredient.create!(name: "eggplant", calories: 100)
+      tomato = Ingredient.create!(name: "tomato", calories: 80)
+      zucc = Ingredient.create!(name: "zucchini", calories: 120)
+      salt = Ingredient.create!(name: "salt", calories: 10)
+      basil = Ingredient.create!(name: "basil", calories: 20)
+      sausage = Ingredient.create!(name: "italian sausage", calories: 200)
+      wine = Ingredient.create!(name: "Cabernet Sauvignon", calories: 150)
+
+      DishIngredient.create!(dish_id: ratatouille.id, ingredient_id: egg.id)
+      DishIngredient.create!(dish_id: ratatouille.id, ingredient_id: basil.id)
+      DishIngredient.create!(dish_id: ratatouille.id, ingredient_id: tomato.id)
+      DishIngredient.create!(dish_id: ratatouille.id, ingredient_id: zucc.id)
+      DishIngredient.create!(dish_id: ratatouille.id, ingredient_id: salt.id)
+
+      DishIngredient.create!(dish_id: spaghetti.id, ingredient_id: sausage.id)
+      DishIngredient.create!(dish_id: spaghetti.id, ingredient_id: tomato.id)
+      DishIngredient.create!(dish_id: spaghetti.id, ingredient_id: wine.id)
+      DishIngredient.create!(dish_id: spaghetti.id, ingredient_id: basil.id)
+      DishIngredient.create!(dish_id: spaghetti.id, ingredient_id: salt.id)
+
+      ingredients = [tomato, basil, salt]
+
+      expect(franc.popular_ingredients).to eq(ingredients)
     end
   end
 end
