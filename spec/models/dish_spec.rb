48f13e105400bc395e913c24ca4d1495ea4e2198
Will Dunlap <dunlapww@gmail.com>
2020-10-29 09:55:45 -0600

    Add dish model test for calories method

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -10,4 +10,17 @@ RSpec.describe Dish, type: :model do
     it {should have_many :dish_ingredients}
     it {should have_many(:ingredients).through(:dish_ingredients)}
   end
+
+  describe 'instance methods' do
+    it '.calories' do
+      @chef1 = Chef.create({name: "Ralph"})
+      @dish1 = @chef1.dishes.create({name: "pasta", description: "noodles and sauce"})
+      @noodles = Ingredient.create({name: "noodles", calories: 100})
+      @sauce = Ingredient.create({name: "sauce", calories: 80})
+      @d1_noodles = DishIngredient.create({dish_id: @dish1.id, ingredient_id: @noodles.id})
+      @d1_sauce = DishIngredient.create({dish_id: @dish1.id, ingredient_id: @sauce.id})
+
+      expect(@dish1.calories).to eq(180)
+    end
+  end
 end
