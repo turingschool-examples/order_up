905df48ac1fa05360ec70abe41619542871a565f
Will Dunlap <dunlapww@gmail.com>
2020-10-29 09:47:16 -0600

    Add many to many test and relationship for dish/ingredients

--- app/models/dish_ingredient.rb
+++ app/models/dish_ingredient.rb
@@ -1,3 +1,4 @@
 class DishIngredient < ApplicationRecord
-  
+  belongs_to :dish
+  belongs_to :ingredient
 end
---
+++ spec/models/dish_ingredient_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe DishIngredient, type: :model do
+  describe 'relationships' do
+    it {should belong_to :dish}
+    it {should belong_to :ingredient}
+  end
+end
+
