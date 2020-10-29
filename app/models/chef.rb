f67c18e570eb2612c4233416a648328016d8fc7e
GregJMitchell <g2mitchell3003@gmail.com>
2020-10-29 10:50:32 -0600

    Attempt xtension

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -3,4 +3,8 @@ class Chef <ApplicationRecord
   has_many :dishes
 
   has_many :ingredients, through: :dishes
+
+  def top_three_ingredients
+    self.ingredients.select('ingredients.name, COUNT(ingredients.name) as count').group("ingredients.name").order(count: :desc).limit(3)
+  end
 end
