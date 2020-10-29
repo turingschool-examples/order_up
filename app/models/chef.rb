ebe771dcbdc505c43eb33f1828ac6340da8b422d
Zach Stearns <zach.stearns92@gmail.com>
2020-10-29 10:30:32 -0600

    Add method for listing all chef's ingredients

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -2,4 +2,9 @@ class Chef <ApplicationRecord
   validates_presence_of :name
   has_many :dishes
 
+  def all_dish_ingredients
+    self.dishes.map do |dish|
+      dish.ingredient_list
+    end.flatten
+  end
 end
