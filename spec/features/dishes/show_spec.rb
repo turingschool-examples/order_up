db944fa99b0346c918935fc484d15fcde59068d9
Kiera Allen <kieraaallen@gmail.com>
2020-10-29 09:20:48 -0600

    Create dish show spec

---
+++ spec/features/dishes/show_spec.rb
@@ -0,0 +1,35 @@
+# As a visitor
+# When I visit a dish's show page
+# I see the dish’s name and description
+# And I see a list of ingredients for that dish
+# And I see the chef's name
+
+require "rails_helper"
+
+describe "As a visitor" do
+  describe "when I visit a dish's show page" do
+    before :each do
+      @chef_1 = Chef.create!(name: "Emeril Lagasse")
+      @dish_1 = @chef_1.dishes.create!(name: "Jalapeno Cornbread", description: "The cornbread has the perfect texture and the jalapenos add a great kick.")
+      @corn = @dish_1.ingredients.create!(name: "corn", calories: 132)
+      @milk = @dish_1.ingredients.create!(name: "milk", calories: 122)
+      @jalapeno = @dish_1.ingredients.create!(name: "jalapeno", calories: 4)
+      @flour = @dish_1.ingredients.create!(name: "flour", calories: 100)
+    end
+
+    it "the visitor sees dish's name and description, list of ingredients, and chef's name" do
+
+      visit "/dishes/#{@dish_1.id}"
+
+      expect(page).to have_content(@dish_1.name)
+      expect(page).to have_content(@dish_1.description)
+
+      expect(page).to have_content(@corn.name)
+      expect(page).to have_content(@milk.name)
+      expect(page).to have_content(@jalapeno.name)
+      expect(page).to have_content(@flour.name)
+
+      expect(page).to have_content(@chef_1.name)
+    end
+  end
+end
