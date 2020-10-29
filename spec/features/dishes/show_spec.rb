45900b880e73304b312d6221b4220dc55df6ccf7
Brett Turing <bjsherman80@gmail.com>
2020-10-29 10:04:01 -0600

    added calorie count to dish show page

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe "dish show page", type: :feature do
   it "can display the dishes attributes" do
     chef_1 = Chef.create!(name: "Master Chef Tingo")
     dish_1 = chef_1.dishes.create!(name: "Shrimp Fried Rice", description: "Hails from the great hills of Japan.")
-    ingredient_1 = Ingredient.create!(name: "Black Dragon Rice")
-    ingredient_2 = Ingredient.create!(name: "Sweet Orange Nectar from the Killer Bee.")
+    ingredient_1 = Ingredient.create!(name: "Black Dragon Rice", calories: 100)
+    ingredient_2 = Ingredient.create!(name: "Sweet Orange Nectar from the Killer Bee", calories: 1000)
     dish_1.ingredients << ingredient_1
     dish_1.ingredients << ingredient_2
     chef_1.dishes << dish_1
@@ -17,5 +17,6 @@ RSpec.describe "dish show page", type: :feature do
     expect(page).to have_content(ingredient_1.name)
     expect(page).to have_content(ingredient_2.name)
     expect(page).to have_content(chef_1.name)
+    expect(page).to have_content("Calories: #{dish_1.calorie_count}")
   end
 end
