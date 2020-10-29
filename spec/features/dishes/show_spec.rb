40d47da93285a136ea5e8531a01f6e49898724c9
Brett Turing <bjsherman80@gmail.com>
2020-10-29 09:36:31 -0600

    create dish show page feature spec

---
+++ spec/features/dishes/show_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "dish show page", type: :feature do
+  it "can display the dishes attributes" do
+    chef_1 = Chef.create!(name: "Master Chef Tingo")
+    dish_1 = chef_1.dishes.create!(name: "Shrimp Fried Rice", description: "Hails from the great hills of Japan.")
+    ingredient_1 = Ingredient.create!(name: "Black Dragon Rice")
+    ingredient_2 = Ingredient.create!(name: "Sweet Orange Nectar from the Killer Bee.")
+    dish_1.ingredients << ingredient_1
+    dish_1.ingredients << ingredient_2
+    chef_1.dishes << dish_1
+
+    visit "/dishes/#{dish_1.id}"
+
+    expect(page).to have_content(dish_1.name)
+    expect(page).to have_content(dish_1.description)
+    expect(page).to have_content(ingredient_1.name)
+    expect(page).to have_content(ingredient_2.name)
+    expect(page).to have_content(chef_1.name)
+  end
+end
