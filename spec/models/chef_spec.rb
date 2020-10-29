9f9c1d2a52176409bb08751a5a6065bfd945ab19
Will Dunlap <dunlapww@gmail.com>
2020-10-29 10:37:48 -0600

    Add model test for chef ingredients

--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -7,4 +7,21 @@ RSpec.describe Chef, type: :model do
   describe "relationships" do
     it {should have_many :dishes}
   end
+
+  describe "instance methods" do
+    it ".ingredients" do
+      @chef1 = Chef.create({name: "Ralph"})
+      @dishr = @chef1.dishes.create({name: "pasta w red sauce", description: "noodles and red sauce"})
+      @dishw = @chef1.dishes.create({name: "pasta w white sauce", description: "noodles and white sauce"})
+      @noodles = Ingredient.create({name: "noodles", calories: 100})
+      @wsauce = Ingredient.create({name: "white sauce", calories: 80})
+      @rsauce = Ingredient.create({name: "red sauce", calories: 75})
+      @dishr_noodles = DishIngredient.create({dish_id: @dishr.id, ingredient_id: @noodles.id})
+      @dishr_rsauce = DishIngredient.create({dish_id: @dishr.id, ingredient_id: @rsauce.id})
+      @dishw_noodles = DishIngredient.create({dish_id: @dishw.id, ingredient_id: @noodles.id})
+      @dishw_wsauce = DishIngredient.create({dish_id: @dishw.id, ingredient_id: @wsauce.id})
+
+      expect(@chef1.ingredients).to eq(["noodles", "red sauce", "white sauce"])
+    end
+  end
 end
