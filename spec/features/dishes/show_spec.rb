d42f85b32bc40776b34db975d4f6cd24e4d351a6
Roberto Basulto <roberto.basulto085@gmail.com>
2020-10-29 11:50:24 -0400

    Added test for ingredients on page and chef

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -2,14 +2,36 @@ require 'rails_helper'
 
 describe "As a visitor" do
   describe "When I visit a dishe's show page" do
-    it "I see a dishe's name and description" do
+    it "I see a dishe's name and description and chef name" do
       gordon = Chef.create({name: "Gordon Ramsey"})
       burger = Dish.create({name: "Cheese Burger", description: "Famous Burger", chef_id: gordon.id})
 
       visit "/dishes/#{burger.id}"
 
+      expect(page).to have_content(gordon.name)
       expect(page).to have_content(burger.name)
       expect(page).to have_content(burger.description)
+    end
+
+    it "I see a list of ingredients for that dish" do
+      gordon = Chef.create({name: "Gordon Ramsey"})
+      burger = Dish.create({name: "Cheese Burger", description: "Famous Burger", chef_id: gordon.id})
+      buns = Ingredient.create({name: "Buns", calories: 60})
+      patty = Ingredient.create({name: 'Patty', calories: 120})
+      tomato = Ingredient.create({name: 'Tomato', calories: 30})
+      onion = Ingredient.create({name: 'Onion', calories: 20})
+      DishIngredient.create({dish_id: burger.id, ingredient_id: buns.id})
+      DishIngredient.create({dish_id: burger.id, ingredient_id: patty.id})
+      DishIngredient.create({dish_id: burger.id, ingredient_id: tomato.id})
+      DishIngredient.create({dish_id: burger.id, ingredient_id: onion.id})
+
+      visit "/dishes/#{burger.id}"
+
+      expect(page).to have_content("Ingredients")
+      expect(page).to have_content(buns.name)
+      expect(page).to have_content(patty.name)
+      expect(page).to have_content(tomato.name)
+      expect(page).to have_content(onion.name)
     end
   end
 end
