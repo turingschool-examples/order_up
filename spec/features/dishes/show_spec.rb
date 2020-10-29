e85413beb19e4ef56a6be62f0f13cf30cb1988ac
Jake Heft <jakeheft@gmail.com>
2020-10-29 09:50:47 -0600

    Add test for total calories

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -45,5 +45,42 @@ describe "As a visitor" do
       end
       expect(page).to have_content("Chef: #{chef.name}")
     end
+
+    it "I see total calories of that dish" do
+      chef = Chef.create!(name: "Bob Newhart")
+      dish = Dish.create!(
+        name: "Spaghetti",
+        description: "Spaghetti and Meatballs",
+        chef_id: chef.id
+      )
+      pasta = Ingredient.create!(
+        name: "Pasta",
+        calories: 150
+      )
+      sauce = Ingredient.create!(
+        name: "Marinara Sauce",
+        calories: 75
+      )
+      meatballs = Ingredient.create!(
+        name: "Meatballs",
+        calories: 300
+      )
+      DishIngredient.create!(
+        dish_id: dish.id,
+        ingredient_id: pasta.id
+      )
+      DishIngredient.create!(
+        dish_id: dish.id,
+        ingredient_id: sauce.id
+      )
+      DishIngredient.create!(
+        dish_id: dish.id,
+        ingredient_id: meatballs.id
+      )
+
+      visit "/dishes/#{dish.id}"
+
+      expect(page).to have_content("Total Calories: #{dish.total_calories}")
+    end
   end
 end
