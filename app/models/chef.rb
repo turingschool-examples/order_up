4fbd88a6e3a6e912cc4803d162ad8c4b265c61b4
Taylor Phillips <taylorscottphillips@gmail.com>
2020-10-29 12:01:18 -0500

    WIP: functionality for user story 3

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -2,4 +2,8 @@ class Chef <ApplicationRecord
   validates_presence_of :name
   has_many :dishes
 
+  def all_ingredients
+    # couldn't figure out an active record way to do this
+    self.dishes.joins(:ingredients).where(ingredient_id: :ingredient_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/dishes/:id', to: 'dishes#show'
+
+  get 'chefs/:id', to: 'chefs#show'
+  get 'chefs/chefs/:id/ingredients', to: 'ingredients#index'
 end
