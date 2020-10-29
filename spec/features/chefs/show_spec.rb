2f0437716177384c6f9f207ef00b6668ad861c39
Eugene Theriault <hybridbassist@gmail.com>
2020-10-29 09:08:58 -0700

    Create test for finding chefs name on chef show page

---
+++ spec/features/chefs/show_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe 'As a visitor' do
+  describe 'when I visit a chefs show page' do
+    before :each do
+      @chef = Chef.create!(name: "Guy Ramsey")
+
+      @dish1 = @chef.dishes.create!(name: 'Burger Bomb',
+                                    description: 'An explosion of flavor!')
+                                    
+      @dish2 = @chef.dishes.create!(name: 'Pizza Pizzazz',
+                                    description: 'Pizzapizzapizzapizza')
+
+      @dish3 = @chef.dishes.create!(name: 'Ravenous Ravioli',
+                                    description: 'Eat it before it eats you!')
+
+      @ingredient = @dish1.ingredients.create!(name: "Ground Beef",
+                                               calories: "100")
+
+      @ingredient2 = @dish2.ingredients.create!(name: "Cheese",
+                                                calories: "50")
+
+      @dish3.ingredients << @ingredient2
+      @dish1.ingredients << @ingredient2
+    end
+
+    it 'has the name of that chef' do
+      visit "/chefs/#{@chef.id}"
+
+      expect(page).to have_content(@chef.name)
+    end
+  end
+end
+
