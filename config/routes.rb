e8ddf2f1a78d97275d419a2de5693707d798d93f
Sage Gonzales <sagegonzales15@gmail.com>
2020-10-29 09:48:49 -0600

    Add route for user story 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #dish
+  get "/dishes/:dish_id", to: "dishes#show"
 end
---
+++ spec/features/dishes/show_spec.rb
@@ -0,0 +1,77 @@
+require 'rails_helper'
+
+RSpec.describe 'As a visitor', type: :feature do
+  describe "When I visit a dish's show page" do
+    
+
+    before(:each) do
+      @chef = Chef.create!(
+        name: "SpongeBob"
+      )
+
+      @burger = Dish.create!(
+        name: "Krabby Patty",
+        description: "Delicious undersea burger",
+        chef_id: @chef.id
+      )
+
+      @fries = Dish.create!(
+        name: "Kelp Fries",
+        description: "Perfect side to a Krabby Patty",
+        chef_id: @chef.id
+      )
+
+      @patty = Ingredient.create!(
+        name: "Burger Patty",
+        calories: 300
+      )
+
+      @lettuce = Ingredient.create!(
+        name: "Lettuce",
+        calories: 10
+      )
+
+      @bun = Ingredient.create!(
+        name: "Burger Bun",
+        calories: 30
+      )
+
+      @kelp = Ingredient.create!(
+        name: "Kelp",
+        calories: 10
+      )
+
+      Recipe.create!(
+        dish_id: @burger.id,
+        ingredient_id: @patty.id 
+      )
+
+      Recipe.create!(
+        dish_id: @burger.id,
+        ingredient_id: @lettuce.id 
+      )
+
+      Recipe.create!(
+        dish_id: @burger.id,
+        ingredient_id: @bun.id 
+      )
+
+      Recipe.create!(
+        dish_id: @fries.id,
+        ingredient_id: @kelp.id 
+      )
+    end 
+    it 'I see a dishes name, description, ingredients, and chefs name' do
+      visit "/dish/#{@burger.id}"
+
+      expect(page).to have_content("Krabby Patty")
+      expect(page).to have_content("Delicious undersea burger")
+      expect(page).to have_content("Ingredients:")
+      expect(page).to have_content("Burger Patty")
+      expect(page).to have_content("Lettuce")
+      expect(page).to have_content("Burger Bun")
+      expect(page).to have_content("Chef: SpongeBob")
+    end
+  end
+  
+end
