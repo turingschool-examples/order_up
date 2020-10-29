d8f180e2f288ec0b05cad72a3dd15d45003284ec
Kiera Allen <kieraaallen@gmail.com>
2020-10-29 10:00:16 -0600

    Create total_calories method

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,8 +1,12 @@
-class Dish <ApplicationRecord
+class Dish < ApplicationRecord
   validates_presence_of :name, :description
 
   belongs_to :chef
 
   has_many :recipes
   has_many :ingredients, through: :recipes
+
+  def total_calories
+    ingredients.sum(:calories)
+  end
 end
--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -5,9 +5,26 @@ RSpec.describe Dish, type: :model do
     it { should validate_presence_of :name }
     it { should validate_presence_of :description }
   end
+
   describe "relationships" do
     it { should belong_to :chef }
     it { should have_many :recipes }
     it { should have_many(:ingredients).through(:recipes) }
+  end
+
+  describe "instance methods" do
+    before :each do
+      @chef_1 = Chef.create!(name: "Emeril Lagasse")
+      @dish_1 = @chef_1.dishes.create!(name: "Jalapeno Cornbread", description: "The cornbread has the perfect texture and the jalapenos add a great kick.")
+      @corn = @dish_1.ingredients.create!(name: "corn", calories: 132)
+      @milk = @dish_1.ingredients.create!(name: "milk", calories: 122)
+      @jalapeno = @dish_1.ingredients.create!(name: "jalapeno", calories: 4)
+      @flour = @dish_1.ingredients.create!(name: "flour", calories: 100)
+
+    end
+      it "#total_calories" do
+
+      expect(@dish_1.total_calories).to eq(358)
+      end
   end
 end
