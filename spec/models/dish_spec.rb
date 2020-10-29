90229042b0f87099c141e9ae7b35d0eb31e15dc9
Kiera Allen <kieraaallen@gmail.com>
2020-10-29 09:26:39 -0600

    Create relationships and related models for ingredients, dishes, and recipes

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Dish, type: :model do
   describe "validations" do
-    it {should validate_presence_of :name}
-    it {should validate_presence_of :description}
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :description }
   end
   describe "relationships" do
-    it {should belong_to :chef}
+    it { should belong_to :chef }
+    it { should have_many :recipes }
+    it { should have_many(:ingredients).through(:recipes) }
   end
 end
