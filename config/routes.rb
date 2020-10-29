f6d9d3ee854bbbd5f2850804d9a60a9b35a616b6
todd-estes <elestes@gmail.com>
2020-10-29 10:59:49 -0600

    Final commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
 
 resources :dishes do  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+resources :chefs do
+  resources :ingredients do
+end
+end
 end
