c69ac5ebd25ffc23a8eefcdd815524c442ca6411
Carson Jardine <carsonjardine@gmail.com>
2020-10-29 09:33:43 -0600

    Create migrations for ingredient and dishingredient

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -5,12 +5,21 @@ describe 'As a visitor' do
     it "I see a list of ingredients for that dish and the chef's name" do
       chef_1 = Chef.create!(name: "Mike Dao")
       dish_1 = Dish.create!({name: "Pancakes", description: "Yummy, fluffy, buttery, goodness", chef_id: chef_1.id})
+      egg = Ingredient.create!(name: "egg")
+      butter = Ingredient.create!(name: "butter")
+      sugar = Ingredient.create!(name: "sugar")
+      DishIngredient.create!({dish_id: dish_1.id, ingredient_id: egg.id})
+      DishIngredient.create!({dish_id: dish_1.id, ingredient_id: butter.id})
+      DishIngredient.create!({dish_id: dish_1.id, ingredient_id: sugar.id})
 
       visit "/dishes/#{dish_1.id}"
 
       expect(page).to have_content(dish_1.name)
       expect(page).to have_content(dish_1.description)
       expect(page).to have_content(chef_1.name)
+      expect(page).to have_content(egg.name)
+      expect(page).to have_content(butter.name)
+      expect(page).to have_content(sugar.name)
     end
   end
 end
