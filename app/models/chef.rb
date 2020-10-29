6deee6e3536aa34b8f0384dab56556a1fcd7e05c
Shaun James <ShaunDaneJames@Gmail.com>
2020-10-29 10:36:19 -0600

    Adds a unique list of all ingredients to a chef show page

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,5 +1,12 @@
-class Chef <ApplicationRecord
+class Chef < ApplicationRecord
   validates_presence_of :name
   has_many :dishes
 
+  def find_ingredients
+    dishes.flat_map do |dish|
+      dish.ingredients.flat_map do |ingredient|
+        ingredient.name
+      end
+    end.uniq
+  end
 end
--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -18,6 +18,35 @@ describe "As a visitor" do
 
       expect(page).to have_content(chef_steve.name)
       expect(page).to have_link("All Ingredients Used")
-    end 
+    end
+
+    it "When I click on 'All Ingredients Used' I see a list
+    of all ingredients this chef uses" do
+      chef_steve = Chef.create!(name: "Steve")
+      pbj = chef_steve.dishes.create!(name: "PeanutButter Jelly",
+                         description: "Most delicious")
+      peanutbutter = pbj.ingredients.create!(name: "PeanutButter",
+                                             calories: 1337)
+      jelly = pbj.ingredients.create!(name: "Jelly",
+                                             calories: 9999)
+      bread = pbj.ingredients.create!(name: "Bread",
+                                             calories: 250)
+      ham_sammy = chef_steve.dishes.create!(name: "Ham Sammy",
+                         description: "Most delicious")
+      ham = ham_sammy.ingredients.create!(name: "Ham",
+                                             calories: 1337)
+      cheese = ham_sammy.ingredients.create!(name: "Cheese",
+                                             calories: 9999)
+      bread = ham_sammy.ingredients.create!(name: "Bread",
+                                             calories: 250)
+
+      visit "/chefs/#{chef_steve.id}"
+      click_link "All Ingredients Used"
+save_and_open_page
+      expect(current_path).to eq("/chefs/#{chef_steve.id}/ingredients")
+      expect(page).to have_content("PeanutButter")
+      expect(page).to have_content("Jelly")
+      expect(page).to have_content("Bread")
+    end
   end
 end
