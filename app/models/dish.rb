4a30821407250e529f41946d0237aeedefe89964
Brian Liu <brian.b.liu@gmail.com>
2020-10-29 10:24:48 -0600

    Complete user story 2

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -3,4 +3,8 @@ class Dish < ApplicationRecord
   belongs_to :chef
   has_many :recipes
   has_many :ingredients, through: :recipes
+
+  def calorie_count
+    self.ingredients.sum(:calories)
+  end
 end
--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -23,7 +23,26 @@ RSpec.describe 'When I visit a dish show page' do
       expect(page).to have_content(dish_1.description)
       expect(page).to have_content(ingredient_1.name)
       expect(page).to have_content(dish_1.chef.name)
-      save_and_open_page
+    end
+
+    it 'I can see the total calorie count for that dish' do
+      chef_1 = Chef.create!(name: 'Chef Scott')
+      ingredient_1 = Ingredient.create!(name: 'Peas', calories: '50')
+      ingredient_2 = Ingredient.create!(name: 'Bacon', calories: '200')
+      ingredient_3 = Ingredient.create!(name: 'Egg', calories: '100')
+      ingredient_4 = Ingredient.create!(name: 'Spaghetti', calories: '300')
+      dish_1 = chef_1.dishes.create!(name: 'Spaghetti Carbonara', description: 'Made with egg, bacon, and peas.')
+      dish_2 = chef_1.dishes.create!(name: 'Eggs and Bacon', description: 'Simple protein breakfast.')
+      Recipe.create!(ingredient: ingredient_1, dish: dish_1)
+      Recipe.create!(ingredient: ingredient_2, dish: dish_1)
+      Recipe.create!(ingredient: ingredient_3, dish: dish_1)
+      Recipe.create!(ingredient: ingredient_4, dish: dish_1)
+      Recipe.create!(ingredient: ingredient_2, dish: dish_2)
+      Recipe.create!(ingredient: ingredient_3, dish: dish_2)
+
+      visit "/dishes/#{dish_1.id}"
+
+      expect(page).to have_content('Total Calories: 650')
     end
   end
 end
