a5a0ed131bb41eb214960c4aeab4d6e8aba366f4
Brett Turing <bjsherman80@gmail.com>
2020-10-29 10:03:32 -0600

    tried to change column but could not so I removed the string version of calories and replaced with integer version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_151731) do
+ActiveRecord::Schema.define(version: 2020_10_29_155906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,9 +32,9 @@ ActiveRecord::Schema.define(version: 2020_10_29_151731) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
-    t.string "calories", default: "0"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "calories"
   end
 
   create_table "recipes", force: :cascade do |t|
