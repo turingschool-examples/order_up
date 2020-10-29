4032e19d010e7491a5f82540037339de0851c9b8
George Soderholm <georgesoderholm@gmail.com>
2020-10-29 10:10:05 -0600

    Adds a model method test to check the unit worthiness of the method total_calories

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -10,4 +10,18 @@ RSpec.describe Dish, type: :model do
     it { should have_many :dish_ingredients }
     it { should have_many(:ingredients).through(:dish_ingredients) }
   end
+
+  describe 'instance methods' do
+    before :each do
+      @murray = Chef.create!(name: 'Murray McMasterful')
+      @spaghetti = @murray.dishes.create!(name: 'Spaghetti', description: 'Delish!')
+      @pasta = @spaghetti.ingredients.create!(name: 'Rigitoni', calories: 10)
+      @sauce = @spaghetti.ingredients.create!(name: 'Red Sauce', calories: 50)
+      @salt = @spaghetti.ingredients.create!(name: 'Salt', calories: 5)
+    end
+
+    it '#total_calories' do
+      expect(@spaghetti.total_calories).to eq(65)
+    end 
+  end
 end
