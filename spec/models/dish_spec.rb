83c3418e7844dca29746d1851a08639f30d41e6b
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-29 09:34:25 -0600

    Add model test to calculate totalt calories for a dish

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -10,4 +10,22 @@ RSpec.describe Dish, type: :model do
     it { should have_many :dish_ingredients }
     it { should have_many(:ingredients).through(:dish_ingredients) }
   end
+
+  describe "instance methods" do
+
+    before :each do
+      @chef = Chef.create!(name: "Chef Casey")
+      @dish = @chef.dishes.create!(name: "Mac n Cheese", description: "Creammy goodness")
+      @cheese = Ingredient.create!(name: "Cheese", calories: 750)
+      @pasta = Ingredient.create!(name: "Pasta", calories: 300)
+      @breadcrumbs = Ingredient.create!(name: "Breadcrumbs", calories: 175)
+      DishIngredient.create!(dish_id: @dish.id, ingredient_id: @cheese.id)
+      DishIngredient.create!(dish_id: @dish.id, ingredient_id: @pasta.id)
+      DishIngredient.create!(dish_id: @dish.id, ingredient_id: @breadcrumbs.id)
+    end
+
+    it "#total_calories" do
+      expect(@dish.total_calories).to eq(1225)
+    end
+  end
 end
