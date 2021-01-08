35651fdaa546c16e64bd920319b26961e9843e00
Catherine Dean <catherinemdean15@gmail.com>
2021-01-08 09:42:22 -0700

    add calories column to indgredients table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_08_161152) do
+ActiveRecord::Schema.define(version: 2021_01_08_164051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2021_01_08_161152) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "calories"
   end
 
   create_table "recipes", force: :cascade do |t|
