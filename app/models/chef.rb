18b20edf61980808fb5973d4f1deb60649e9b511
Catherine Dean <catherinemdean15@gmail.com>
2021-01-08 11:18:47 -0700

    create top_3_ingredients action in chef show

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -2,4 +2,13 @@ class Chef <ApplicationRecord
   validates_presence_of :name
   has_many :dishes
 
+  def top_3_ingredients
+    Ingredient.joins(:dishes)
+              .where("chef_id = #{self.id}")
+              .select('ingredients.*, count(ingredients) as times_used')
+              .order('times_used DESC')
+              .group(:id)
+              .limit(3)
+  end
+
 end
