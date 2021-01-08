08d5eb43c5f558beef5173685269b1b9856cfa14
Phil McCarthy <philmcc2631@gmail.com>
2021-01-08 11:28:22 -0700

    Add top_three_ingredients method, test passing

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -2,4 +2,13 @@ class Chef <ApplicationRecord
   validates_presence_of :name
   has_many :dishes
   has_many :ingredients, through: :dishes
+  
+  def top_three_ingredients
+    ingredients
+    .select('ingredients.*, count(ingredients) as ingred_usage')
+    .group(:id)
+    .order('ingred_usage desc')
+    .limit(3)
+  end
 end
+  
