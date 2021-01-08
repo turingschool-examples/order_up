aa4428a3a6f3ba0dfb1f27f2ed9a64045a71d027
Miquel <foymikek@gmail.com>
2021-01-08 10:33:18 -0700

    Add passing ingredient class method total_calories

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -10,4 +10,5 @@ RSpec.describe Dish, type: :model do
     it { should have_many :dish_ingredients }
     it { should have_many(:ingredients).through(:dish_ingredients) }
   end
+
 end
--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe Ingredient, type: :model do
     it { should have_many :dish_ingredients }
     it { should have_many(:dishes).through(:dish_ingredients) }
   end
+  describe "model methods" do
+    it "can count total calories" do
+      Ingredient.create(name: "Ham", calories: 50)
+      Ingredient.create(name: "Mustard", calories: 5)
+      Ingredient.create(name: "Bun", calories: 120)
+
+      expect(Ingredient.total_calories).to eq(175)
+
+    end
+  end
 end
