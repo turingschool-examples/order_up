afef2b795333975fec21e182de9ac44f9c070854
Grayson Myers <graymyers10@gmail.com>
2021-01-08 10:04:10 -0700

    Add tests for through relations of dishes and ingredients

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 describe Ingredient, type: :model do
   describe "relations" do
     it {should have_many :ingredient_dishes}
+    it {should have_many(:dishes).through(:ingredient_dishes)}
   end
 end
