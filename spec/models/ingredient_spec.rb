6c49395e3673d091254dba85a7a1f5e55f6529ff
Grayson Myers <graymyers10@gmail.com>
2021-01-08 09:53:00 -0700

    Add test for relationship between ingredient and ingredient dishes

---
+++ spec/models/ingredient_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe Ingredient, type: :model do
+  describe :relations do
+    it {should have_many :ingredient_dishes}
+    it {should have_many(:dishes).through(:ingredient_dishes)}
+
+  end
+end
