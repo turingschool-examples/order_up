827b4660999bb4ab801739bed5b3c067b5d98294
Isabelle Villasenor <villasenori@outlook.com>
2021-01-08 11:33:03 -0700

    Add Top 3 Method to Chef Model

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -3,4 +3,11 @@ class Chef <ApplicationRecord
   has_many :dishes, dependent: :destroy
   has_many :ingredients, through: :dishes
 
+  def top_three
+    ingredients
+      .select('ingredients.name, count(ingredients.name) as ingredient_count')
+      .group('ingredients.name')
+      .order(ingredient_count: :desc)
+      .limit(3)
+  end
 end
