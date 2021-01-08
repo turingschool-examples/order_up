8ea4f1b740c32c8421aa9e487d346ed8bfff5674
ninesky00 <jiangzho@gmail.com>
2021-01-08 11:51:53 -0600

    add test display total calorie count

--- spec/features/dish/show_spec.rb
+++ spec/features/dish/show_spec.rb
@@ -5,7 +5,9 @@ RSpec.describe "dish show page" do
     @chef1 = Chef.create!(name: "mario")
     @dish1 = Dish.create!(name: "pasta", description: "italian", chef: @chef1)
     @ingredient1 = Ingredient.create!(name: "tomatoes", calories: 10)
+    @ingredient2 = Ingredient.create!(name: "noddles", calories: 50)
     DishIngredient.create!(dish: @dish1, ingredient: @ingredient1)
+    DishIngredient.create!(dish: @dish1, ingredient: @ingredient2)
     visit dish_path(@dish1.id)
   end
 
@@ -15,10 +17,21 @@ RSpec.describe "dish show page" do
   end
 
   it "displays a list of ingredients for that dish" do 
-    expect(page).to have_content(@ingredient1.name)
+    within("#ingredients") do 
+      expect(page).to have_content(@ingredient1.name)
+      expect(page).to have_content(@ingredient2.name)
+    end
   end
 
   it "display the chef's name" do 
-    expect(page).to have_content(@chef1.name)
+    within("#chef") do 
+      expect(page).to have_content(@chef1.name)
+    end
+  end
+
+  it "displays dish's total calorie count" do 
+    within("#dish-details") do
+      expect(page).to have_content(@ingredient1.calories + @ingredient2.calories)
+    end
   end
 end
