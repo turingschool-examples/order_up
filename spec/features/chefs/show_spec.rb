39e697854f5274e6d31bf5601b19ef7100b424f3
Max Ribbans <ribbans.max@gmail.com>
2021-01-08 12:54:26 -0500

    Add chef show page

---
+++ spec/features/chefs/show_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe "Chef show page" do
+  it "displays relevant information" do
+    chef = Chef.create(name: "Rordan Gamsey")
+    dish2 = chef.dishes.create(name: "Water", description: "A glass of water")
+    dish = chef.dishes.create(name: "Ice Water", description: "A glass of ice water")
+    lemon = Ingredient.create(name: "Artisinal slice of lemon", calories: 6)
+    h = Ingredient.create(name: "H", calories: 0)
+    o = Ingredient.create(name: "O", calories: 0)
+    ice = Ingredient.create(name: "Ice", calories: 1)
+    Component.create(dish_id: dish.id, ingredient_id: h.id, amount: 2)
+    Component.create(dish_id: dish.id, ingredient_id: o.id, amount: 1)
+    Component.create(dish_id: dish.id, ingredient_id: lemon.id, amount: 1)
+    Component.create(dish_id: dish2.id, ingredient_id: h.id, amount: 2)
+    Component.create(dish_id: dish2.id, ingredient_id: o.id, amount: 1)
+    Component.create(dish_id: dish2.id, ingredient_id: lemon.id, amount: 1)
+    Component.create(dish_id: dish2.id, ingredient_id: ice.id, amount: 4)
+
+    visit chef_path(chef)
+
+    expect(page).to have_content("Chef: #{chef.name}")
+    expect(page).to have_link("Ingredients", href: chef_ingredients_path(chef))
+  end
+end
