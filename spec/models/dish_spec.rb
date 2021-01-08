64fb2972d73daa3ae4153160a8a9311edd3ff590
Sam Yeo <yeosamuel95@gmail.com>
2021-01-08 10:56:09 -0700

    Create model test to get chef name from dish

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Dish, type: :model do
+  before :each do
+    @chef = Chef.create!(name: 'Gordon Lambsauce')
+    @dish_1 = @chef.dishes.create!(name: 'Idiot Sandwich', description: 'Tasty meal consisting of two breads and an idiot')
+    @ingredient_1 = Ingredient.create!(name: 'Bread Loaf', calories: 100)
+  end
   describe "validations" do
     it {should validate_presence_of :name}
     it {should validate_presence_of :description}
@@ -9,5 +14,10 @@ RSpec.describe Dish, type: :model do
     it {should belong_to :chef}
     it {should have_many :dish_ingredients}
     it {should have_many(:ingredients).through(:dish_ingredients)}
+  end
+  describe 'instance methods' do
+    it '#chef_name' do
+      expect(@dish_1.chef_name).to eq(@chef.name)
+    end
   end
 end
