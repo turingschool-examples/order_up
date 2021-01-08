191dd0763391a7f3fd0fed0c329cc8b4e8e3df05
Isabelle Villasenor <villasenori@outlook.com>
2021-01-08 09:53:21 -0700

    Add Relationship Testing to Dish Model Spec

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe Dish, type: :model do
-  describe "validations" do
-    it {should validate_presence_of :name}
-    it {should validate_presence_of :description}
+describe Dish do
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :description }
   end
-  describe "relationships" do
-    it {should belong_to :chef}
+  
+  describe 'relationships' do
+    it { should belong_to :chef }
+    it { should have_many :dish_ingredients }
+    it { should have_many(:ingredients).through(:dish_ingredients) }
   end
 end
