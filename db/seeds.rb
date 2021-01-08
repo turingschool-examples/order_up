20fb53c099ac007a98de5e4097c1fd82a0bf64e9
Ely Hess <Ely.hess@me.com>
2021-01-08 09:22:46 -0700

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+@bob = Chef.create!(name: "Bob")
+
+@dish_1 = @bob.dishes.create!(name: "Dish 1", description: "particularly delicious")
+@dish_2 = @bob.dishes.create!(name: "Dish 2", description: "xtra delicious")
+@dish_3 = @bob.dishes.create!(name: "Dish 3", description: "massive delicious")
+
+@ingredient_1 = Ingredient.create!(name: "A")
+@ingredient_2 = Ingredient.create!(name: "B")
+@ingredient_3 = Ingredient.create!(name: "C")
+@ingredient_4 = Ingredient.create!(name: "D")
+
+Recipe.create!(dish: @dish_1, ingredient: @ingredient_1)
+Recipe.create!(dish: @dish_1, ingredient: @ingredient_2)
+Recipe.create!(dish: @dish_1, ingredient: @ingredient_3)
+
+Recipe.create!(dish: @dish_2, ingredient: @ingredient_1)
+Recipe.create!(dish: @dish_2, ingredient: @ingredient_2)
+
+Recipe.create!(dish: @dish_3, ingredient: @ingredient_1)
+Recipe.create!(dish: @dish_3, ingredient: @ingredient_2)
+Recipe.create!(dish: @dish_3, ingredient: @ingredient_3)
+Recipe.create!(dish: @dish_3, ingredient: @ingredient_4)
