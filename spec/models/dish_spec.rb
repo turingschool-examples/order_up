3aab6c5aa60154ebf794a3afe982a93ca1508f1b
ninesky00 <jiangzho@gmail.com>
2021-01-08 11:56:16 -0600

    add test instance method calories for Dish class

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -10,4 +10,17 @@ RSpec.describe Dish, type: :model do
     it {should have_many :dish_ingredients}
     it {should have_many :ingredients}
   end
+
+  describe "instance methods" do 
+    it "calories" do 
+      chef1 = Chef.create!(name: "mario")
+      dish1 = Dish.create!(name: "pasta", description: "italian", chef: chef1)
+      ingredient1 = Ingredient.create!(name: "tomatoes", calories: 10)
+      ingredient2 = Ingredient.create!(name: "noddles", calories: 50)
+      DishIngredient.create!(dish: dish1, ingredient: ingredient1)
+      DishIngredient.create!(dish: dish1, ingredient: ingredient2)
+
+      expect(dish1.calories).to eq(60)
+    end
+  end
 end
