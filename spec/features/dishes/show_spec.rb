6824721ad66f2bf134d65601c1f82a1e1d03b4cd
Vanessa Arruda <wanessaarruda@gmail.com>
2021-01-08 10:11:22 -0700

    Add total_calories feature test for dish show page

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -20,19 +20,25 @@ describe 'As a visitor' do
       DishIngredient.create!(dish_id: @dish_2.id, ingredient_id: @ingredient_5.id)
       DishIngredient.create!(dish_id: @dish_2.id, ingredient_id: @ingredient_6.id)
     end
-    it 'Shows me the dish name and description, list of ingredients and chefs name' do
+    # it 'Shows me the dish name and description, list of ingredients and chefs name' do
+    #   visit dish_path(@dish_1)
+    #
+    #   expect(page).to have_content(@dish_1.name)
+    #   expect(page).to_not have_content(@dish_2.name)
+    #   expect(page).to have_content(@dish_1.description)
+    #   expect(page).to have_link(@dish_1.chef.name)
+    #
+    #   within(".dish_ingredients") do
+    #   expect(page).to have_link(@ingredient_1.name)
+    #   expect(page).to have_link(@ingredient_2.name)
+    #   expect(page).to have_link(@ingredient_3.name)
+    #   expect(page).to have_link(@chef_1.name)
+    #   end
+    # end
+    it 'Shows the dish total calories' do
       visit dish_path(@dish_1)
 
-      expect(page).to have_content(@dish_1.name)
-      expect(page).to_not have_content(@dish_2.name)
-      expect(page).to have_content(@dish_1.description)
-
-      within(".dish_ingredients") do
-      expect(page).to have_link(@ingredient_1.name)
-      expect(page).to have_link(@ingredient_2.name)
-      expect(page).to have_link(@ingredient_3.name)
-      expect(page).to have_link(@chef_1.name)
-      end
+      expect(page).to have_content(@dish_1.total_calories)
     end
   end
 end
