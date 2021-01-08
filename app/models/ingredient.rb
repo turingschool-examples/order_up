3143fabbcd7e9b29407bb6de181383db59c252c4
Isabelle Villasenor <villasenori@outlook.com>
2021-01-08 10:10:40 -0700

    Add Dependent Destroy

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,6 +2,6 @@ class Ingredient < ApplicationRecord
   validates_presence_of :name
   validates :calories, numericality: { greater_than: 0 }
 
-  has_many :dish_ingredients
+  has_many :dish_ingredients, dependent: :destroy
   has_many :dishes, through: :dish_ingredients
 end
