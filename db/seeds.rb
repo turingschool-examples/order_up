ea0ede747abf8d301de6abf7c8dcd2a86b423d41
Isabelle Villasenor <villasenori@outlook.com>
2021-01-08 09:57:09 -0700

    Add Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Chef.destroy_all
+Dish.destroy_all
+Ingredient.destroy_all
+DishIngredient.destroy_all
+
+@c1 = Chef.create(name: 'Isabelle')
+
+@d1 = @c1.dishes.create(name: 'Ramen', description: 'Yum Yum Noods')
+
+@i1 = Ingredient.create!(name: 'Noodles', calories: 100)
+@i2 = Ingredient.create!(name: 'Egg', calories: 30)
+@i3 = Ingredient.create!(name: 'Green Onions', calories: 8)
+@i4 = Ingredient.create!(name: 'Enoki', calories: 18)
+
+DishIngredient.create!(dish: @d1, ingredient: @i1)
+DishIngredient.create!(dish: @d1, ingredient: @i2)
+DishIngredient.create!(dish: @d1, ingredient: @i3)
+DishIngredient.create!(dish: @d1, ingredient: @i4)
