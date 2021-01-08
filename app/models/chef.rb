69e4459b7d0c480413c006ece8ff8e2b2129d1a5
Adam Etzion <adametzion90@gmail.com>
2021-01-08 11:23:20 -0700

    Add extension attempt

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -3,4 +3,7 @@ class Chef <ApplicationRecord
   has_many :dishes
   has_many :ingredients, through: :dishes
 
+  def top_three
+    
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+@chef1 = Chef.create(name: "Chef Von Winkle")
+@dish1 = Dish.create(name: "apple pie", description: "see name", chef: @chef1)
+@dish2 = Dish.create(name: "pommes frites", description: "its delicious just eat it", chef: @chef1)
+@ingredient1 = Ingredient.create(name: "pommes", calories: 300)
+@ingredient2 = Ingredient.create(name: "frites", calories: 200)
+@recipe1 = Recipe.create(dish: @dish2, ingredient: @ingredient1)
+@recipe2 = Recipe.create(dish: @dish2, ingredient: @ingredient2)
