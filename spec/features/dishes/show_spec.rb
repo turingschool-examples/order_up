c17bac9aba82dd9248f31bbeab72fe38f8f5262e
Catherine Dean <catherinemdean15@gmail.com>
2021-01-08 09:48:22 -0700

    create test for displaying total calories on dish show

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe 'Dishes show page' do
     @chef1 = Chef.create!(name: "Catherine")
     @dish1 = Dish.create!(name: "Mac and Cheese", description: "So cheesy!", chef_id: @chef1.id )
     @dish2 = Dish.create!(name: "Cheese Fries", description: "So cheesy! So fries!", chef_id: @chef1.id )
-    @ingredient1 = Ingredient.create!(name: "Mac")
-    @ingredient2 = Ingredient.create!(name: "Cheese")
-    @ingredient3 = Ingredient.create!(name: "Fries")
+    @ingredient1 = Ingredient.create!(name: "Mac", calories: 101)
+    @ingredient2 = Ingredient.create!(name: "Cheese", calories: 286)
+    @ingredient3 = Ingredient.create!(name: "Fries", calories: 223)
     Recipe.create!(ingredient_id: @ingredient1.id, dish_id: @dish1.id)
     Recipe.create!(ingredient_id: @ingredient2.id, dish_id: @dish1.id)
     Recipe.create!(ingredient_id: @ingredient3.id, dish_id: @dish2.id)
@@ -31,6 +31,10 @@ RSpec.describe 'Dishes show page' do
 
   it "displays the chef's name" do
     expect(page).to have_content(@chef1.name)
+  end
+
+  it "displays the total calorie count for the dish" do
+    expect(page).to have_content(@dish.calorie_count)
   end
 
 end
