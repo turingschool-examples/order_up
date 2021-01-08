396441a09bea8c62f4d915483cd9279a8520578e
ninesky00 <jiangzho@gmail.com>
2021-01-08 11:30:23 -0600

    roll back test database to correct type, add relationships to models

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,4 +1,6 @@
-class Dish <ApplicationRecord
-  validates_presence_of :name, :description
+class Dish < ApplicationRecord
   belongs_to :chef
+  has_many :dish_ingredients
+  has_many :ingredients, through: :dish_ingredients
+  validates_presence_of :name, :description
 end
