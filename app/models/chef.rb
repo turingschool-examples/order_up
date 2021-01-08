b6d8f938a4cbe2777c251f93c5e01f0272f5169d
James Fox-Collis <jlfoxcollis@gmail.com>
2021-01-08 11:29:12 -0700

    updated styles and refactored methods

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -3,10 +3,6 @@ class Chef <ApplicationRecord
   has_many :dishes, dependent: :destroy
   has_many :ingredients, through: :dishes
 
-  def unique_ingredients
-    ingredients.distinct
-  end
-
   def top_3_ingredients
     ingredients.select('ingredients.*, count(dishes) AS count').group(:id).order('count DESC').limit(3)
   end
