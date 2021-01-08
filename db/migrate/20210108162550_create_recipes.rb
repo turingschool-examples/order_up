acecd5ff8111d77418f44227db9bef8356ff6573
Adam Etzion <adametzion90@gmail.com>
2021-01-08 09:37:01 -0700

    Correct migrations and migrate

--- db/migrate/20210108162550_create_recipes.rb
+++ db/migrate/20210108162550_create_recipes.rb
@@ -2,7 +2,7 @@ class CreateRecipes < ActiveRecord::Migration[5.2]
   def change
     create_table :recipes do |t|
       t.references :dish, foreign_key: true
-      t.references :recipe, foreign_key: true
+      t.references :ingredient, foreign_key: true
     end
   end
 end
