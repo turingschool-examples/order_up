86bc7d77c0a61d21278223b1de3b577b03fc882d
Cydnee Owens <cks410@yahoo.com>
2021-01-08 11:47:28 -0700

    Start extension problem

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -9,14 +9,24 @@ RSpec.describe 'As a visitor', type: :feature do
       # Dishes:
       @dish1     = Dish.create!(name: 'Sphaghetti', description: 'Sphaghetti noodles with red marinara meat sauce', chef_id: @chef1.id)
       @dish2     = Dish.create!(name: 'Apple Cake', description: 'German Apfel Torte with creme anglaise', chef_id: @chef2.id)
+      @dish3     = Dish.create!(name: 'Lemon Cake', description: 'Zesty and Citrusy with buttery crust', chef_id: @chef2.id)
       # Ingredients:
       @apple     = Ingredient.create!(name:'Apple', calories: 35)
+      @lemon     = Ingredient.create!(name:'Lemon', calories: 35)
       @noodle    = Ingredient.create!(name:'Spaghetti Noodle', calories: 135)
       @sauce     = Ingredient.create!(name:'Sauce', calories: 200)
       @pie_crust = Ingredient.create!(name:'Pie Crust', calories: 150)
+      @cinnamon  = Ingredient.create!(name:'Cinnamon', calories: 0)
+      @butter    = Ingredient.create!(name:'Butter', calories: 150)
       # Dish Ingredients:
       @apple_pie1 = DishIngredient.create!(dish_id: @dish2.id, ingredient_id: @apple.id)
       @apple_pie2 = DishIngredient.create!(dish_id: @dish2.id, ingredient_id: @pie_crust.id)
+      @apple_pie3 = DishIngredient.create!(dish_id: @dish2.id, ingredient_id: @cinnamon.id)
+      @apple_pie4 = DishIngredient.create!(dish_id: @dish2.id, ingredient_id: @butter.id)
+      @lemon_cake1 = DishIngredient.create!(dish_id: @dish3.id, ingredient_id: @lemon.id)
+      @lemon_cake3 = DishIngredient.create!(dish_id: @dish3.id, ingredient_id: @pie_crust.id)
+      @lemon_cake3 = DishIngredient.create!(dish_id: @dish3.id, ingredient_id: @cinnamon.id)
+      @lemon_cake4 = DishIngredient.create!(dish_id: @dish3.id, ingredient_id: @butter.id)
       @spaghetti1 = DishIngredient.create!(dish_id: @dish1.id, ingredient_id: @noodle.id)
       @spaghetti2 = DishIngredient.create!(dish_id: @dish1.id, ingredient_id: @sauce.id)
     end
@@ -32,7 +42,15 @@ RSpec.describe 'As a visitor', type: :feature do
       expect(current_path).to eq(chef_ingredients_path(@chef1.id))
       expect(page).to have_content("Ingredient Name: #{@noodle.name}")
       expect(page).to have_content("Ingredient Name: #{@sauce.name}")
-      save_and_open_page
     end
+    # # Extension
+    # it 'I see the three most popular ingredients that the chef uses in their dishes' do
+    #   visit chef_path(@chef2.id)
+
+    #   expect(page).to have_content('3 Most Popular Ingredients')
+    #   expect(page).to have_content(@butter.name)
+    #   expect(page).to have_content(@cinnamon.name)
+    #   expect(page).to have_content(@pie_crust.name)
+    # end
   end
 end
--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -1,10 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Chef, type: :model do
-  describe "validations" do
-    it {should validate_presence_of :name}
+  before(:each) do
+    # Chefs: 
+    @chef1     = Chef.create!(name: "Henry Dapasta")
+    @chef2     = Chef.create!(name: "Molly LaBaker")
+    # Dishes:
+    @dish1     = Dish.create!(name: 'Sphaghetti', description: 'Sphaghetti noodles with red marinara meat sauce', chef_id: @chef1.id)
+    @dish2     = Dish.create!(name: 'Apple Cake', description: 'German Apfel Torte with creme anglaise', chef_id: @chef2.id)
+    # Ingredients:
+    @apple     = Ingredient.create!(name:'Apple', calories: 35)
+    @noodle    = Ingredient.create!(name:'Spaghetti Noodle', calories: 135)
+    @sauce     = Ingredient.create!(name:'Sauce', calories: 200)
+    @pie_crust = Ingredient.create!(name:'Pie Crust', calories: 150)
+    # Dish Ingredients:
+    @apple_cake1 = DishIngredient.create!(dish_id: @dish2.id, ingredient_id: @apple.id)
+    @apple_cake2 = DishIngredient.create!(dish_id: @dish2.id, ingredient_id: @pie_crust.id)
+    @spaghetti1 = DishIngredient.create!(dish_id: @dish1.id, ingredient_id: @noodle.id)
+    @spaghetti2 = DishIngredient.create!(dish_id: @dish1.id, ingredient_id: @sauce.id)
   end
+
   describe "relationships" do
     it {should have_many :dishes}
+  end
+   # User Story 3
+  it 'Can find the ingredients used in chefs dishes' do
+    expect(@chef2.find_ingredients).to eq(['Apple', 'Pie Crust'])
   end
 end
