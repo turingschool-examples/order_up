c810b2bba942aa314415d7639c71615b1bedb1b1
Adam Etzion <adametzion90@gmail.com>
2021-01-08 09:40:09 -0700

    Add dish show page tests

---
+++ spec/features/dishes/show_spec.rb
@@ -0,0 +1,18 @@
+require 'rails-helper'
+
+RSpec.describe "As a visitor" do
+    describe "when i visit a dish's show page" do
+        it "I see the dish's name and description" do
+        end
+
+        it "I see a list of ingredients for that dish" do
+        end
+
+        it "I see the chef's name" do
+        end
+
+        it "I see the total calorie count for that dish" do
+        end
+
+    end
+end
