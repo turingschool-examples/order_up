29b4c6a400f8c52c21286701c8c3bc7015dbf760
Grayson Myers <graymyers10@gmail.com>
2021-01-08 10:50:43 -0700

    Add calorie count to dish show page view

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -5,6 +5,8 @@ class Dish <ApplicationRecord
   has_many :ingredients, through: :ingredient_dishes
 
   def calorie_count
-    Ingredient.joins(:ingredient_dishes).select("ingredients.name, ingredient_dishes.amount, ingredients.calories").sum("ingredient_dishes.amount * ingredients.calories")
+    Ingredient.joins(:ingredient_dishes)
+    .select("ingredients.name, ingredient_dishes.amount, ingredients.calories")
+    .sum("ingredient_dishes.amount * ingredients.calories")
   end
 end
