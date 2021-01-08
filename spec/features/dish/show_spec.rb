a89949f9ba4607620d48d09c1e48c23a08995c7d
ninesky00 <jiangzho@gmail.com>
2021-01-08 11:32:54 -0600

    add test to display ingredients within that dish

--- spec/features/dish/show_spec.rb
+++ spec/features/dish/show_spec.rb
@@ -4,11 +4,17 @@ RSpec.describe "dish show page" do
   before :each do 
     @chef1 = Chef.create!(name: "mario")
     @dish1 = Dish.create!(name: "pasta", description: "italian", chef: @chef1)
+    @ingredient1 = Ingredient.create!(name: "tomatoes", calories: 10)
+    DishIngredient.create!(dish: @dish1, ingredient: @ingredient1)
     visit dish_path(@dish1.id)
   end
 
   it "displays dish name and description" do 
     expect(page).to have_content(@dish1.name)
     expect(page).to have_content(@dish1.description)
+  end
+
+  it "displays a list of ingredients for that dish" do 
+    expect(page).to have_content(@ingredient1.name)
   end
 end
