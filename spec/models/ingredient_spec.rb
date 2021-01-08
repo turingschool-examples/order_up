7bc2932ea13560d901a5f6c79d12f4f06a9e3c28
Saundra <saundracatalina@gmail.com>
2021-01-08 10:30:46 -0700

    Add test for calorie_total method

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -9,4 +9,13 @@ describe Ingredient, type: :model do
     it { should have_many :dish_ingredients }
     it { should have_many(:dishes).through(:dish_ingredients) }
   end
+  describe "calorie_total" do
+    ingr_1 = Ingredient.create!(name: "beef patty", calories: 800)
+    ingr_2 = Ingredient.create!(name: "gouda cheese", calories: 300)
+    ingr_3 = Ingredient.create!(name: "onion", calories: 50)
+    ingr_4 = Ingredient.create!(name: "bun", calories: 200)
+    it "can count the total calories in a group of ingredients" do
+      expect(Ingredient.calorie_total).to eq(1350)
+    end
+  end
 end
