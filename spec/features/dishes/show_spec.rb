1bc906e6f3ba5783d6eaee19af678a393b07d0b2
Saundra <saundracatalina@gmail.com>
2021-01-08 10:17:48 -0700

    Add test can see dishe's name, descr, ingredients, and chef

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe "dish show page" do
   before :each do
     @chef_1 = Chef.create!(name: "Bob")
-    @dish_1 = Dish.create!(name: "Gouda Burger", description: "delicious goodness", chef_id: "#{@chef_1.id}")
+    @dish_1 = Dish.create!(name: "Gouda Burger", description: "Delicious goodness", chef_id: "#{@chef_1.id}")
     @ingr_1 = Ingredient.create!(name: "beef patty", calories: 800)
     @ingr_2 = Ingredient.create!(name: "gouda cheese", calories: 300)
     @ingr_3 = Ingredient.create!(name: "onion", calories: 50)
@@ -13,5 +13,23 @@ describe "dish show page" do
     @dish_1.ingredients << @ingr_3
     @dish_1.ingredients << @ingr_4
   end
-  
+  it "can see the dish's name, description, list of ingredients for that dish, and chef's name" do
+    visit "/dishes/#{@dish_1.id}"
+
+    within("#dish-info") do
+      expect(page).to have_content(@dish_1.name)
+      expect(page).to have_content(@dish_1.description)
+    end
+
+    within("#ingredients") do
+      expect(page).to have_content(@ingr_1.name)
+      expect(page).to have_content(@ingr_2.name)
+      expect(page).to have_content(@ingr_3.name)
+      expect(page).to have_content(@ingr_4.name)
+    end
+
+    within("#chef") do
+      expect(page).to have_content(@chef_1.name)
+    end
+  end
 end
