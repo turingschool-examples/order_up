2e103cace817b383e6b08e3d054e3bed9f472388
Grayson Myers <graymyers10@gmail.com>
2021-01-08 09:53:10 -0700

    Fix test

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -3,7 +3,5 @@ require 'rails_helper'
 describe Ingredient, type: :model do
   describe :relations do
     it {should have_many :ingredient_dishes}
-    it {should have_many(:dishes).through(:ingredient_dishes)}
-
   end
 end
