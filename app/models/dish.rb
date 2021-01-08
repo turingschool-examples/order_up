be82c319eed87b44bceee0f82416f6091a34d352
Grayson Myers <graymyers10@gmail.com>
2021-01-08 10:05:03 -0700

    Add through relations for ingredients and dishes

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -2,4 +2,5 @@ class Dish <ApplicationRecord
   validates_presence_of :name, :description
   belongs_to :chef
   has_many :ingredient_dishes
+  has_many :ingredients, through: :ingredient_dishes
 end
