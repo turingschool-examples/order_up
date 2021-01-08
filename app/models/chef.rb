700ee6d4304c98858c90ae104ef94a51f5e0e934
Grayson Myers <graymyers10@gmail.com>
2021-01-08 11:55:31 -0700

    Update unique_ingredients method to return strings and make index page for ingredients use this

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -2,4 +2,13 @@ class Chef <ApplicationRecord
   validates_presence_of :name
   has_many :dishes
   has_many :ingredients, through: :dishes
+
+  def top_ingredients
+    ingredients.select("ingredients.name, count(ingredients) as num").group("ingredients.name").order("num DESC").limit(3)
+  end
+
+  def unique_ingredients
+    ingredients.pluck(:name).uniq
+    #most efficient code to get unqiue names from records, uses some ruby
+  end
 end
