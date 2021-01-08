400d3023c5f043e0226b6d35f87f62d49667a39b
Saundra <saundracatalina@gmail.com>
2021-01-08 09:43:00 -0700

    Add relationship tests for all models

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -6,6 +6,7 @@ describe Ingredient, type: :model do
     it { should validate_presence_of :calories }
   end
   describe "relationships" do
-
+    it { should have_many :dish_ingredients }
+    it { should have_many(:dishes).through(:dish_ingredients) }
   end
 end
