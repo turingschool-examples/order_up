26b5e3e8b6bd82795122c462bd8e84fc4154ddd9
Saundra <saundracatalina@gmail.com>
2021-01-08 12:03:21 -0700

    Add test for unique_ingredients method in ingredients model

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -18,4 +18,14 @@ describe Ingredient, type: :model do
       expect(Ingredient.calorie_total).to eq(1350)
     end
   end
+  describe "unique_ingredients" do
+    ingr_1 = Ingredient.create!(name: "beef patty", calories: 800)
+    ingr_2 = Ingredient.create!(name: "gouda cheese", calories: 300)
+    ingr_3 = Ingredient.create!(name: "onion", calories: 50)
+    ingr_4 = Ingredient.create!(name: "bun", calories: 200)
+    ingr_5 = Ingredient.create!(name: "bun", calories: 200)
+    it "can show a list of ingredients without duplicates" do
+      expect(Ingredient.unique_ingredients).to eq([ingr_1.name, ingr_2.name, ingr_3.name, ingr_4.name])
+    end
+  end
 end
