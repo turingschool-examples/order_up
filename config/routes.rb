2af07770e94d9b37dac7cd1b80ac837f5379e7c8
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-02-26 12:47:23 -0600

    Add functionality to display a chef's ingredients on index page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :dishes, only: [:show]
+  resources :chefs, only: [:show]
+
+  get "/chefs/:id/ingredients", to: "ingredients#index", as: "chefs_ingredients"
 end
