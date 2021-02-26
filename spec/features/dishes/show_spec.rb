693d7c0f6dc4b899b2d7345593d5ac41f55bd028
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-02-26 11:40:37 -0600

    Fix test spec for show page, all test pass.

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe "Dish Show Page" do
     @chef1 = Chef.create!(name: "Peter")
     @dish1 = @chef1.dishes.create!(name: "Mac n Cheese", description: "Delicious!, but high in calories")
     @ingredient1 = @dish1.ingredients.create!(name: "Elbow Pasta", calories: 80)
-    @ingredient2 = @dish1.ingredients.create!(name: "Cheese", calories: 180)
+    @ingredient2 = @dish1.ingredients.create!(name: "Cheddar Cheese", calories: 180)
     @ingredient3 = @dish1.ingredients.create!(name: "Butter", calories: 150)
 
     @chef2 = Chef.create!(name: "Tony")
     @dish2 = @chef2.dishes.create!(name: "Cheese Pizza", description: "Best Pizza in town")
     @ingredient4 = @dish2.ingredients.create!(name: "Dough", calories: 120)
-    @ingredient5 = @dish2.ingredients.create!(name: "Cheese", calories: 180)
+    @ingredient5 = @dish2.ingredients.create!(name: "Mozzarella Cheese", calories: 160)
     @ingredient6 = @dish2.ingredients.create!(name: "Pizza Sauce", calories: 100)
   end
   describe "As a visitor" do
@@ -46,8 +46,8 @@ RSpec.describe "Dish Show Page" do
       it "I see the total calorie count for that dish" do
         visit "/dishes/#{@dish1.id}"
           within "#dish-calories" do
-            expect(page).to have_content(@dish1.calories)
-            expect(page).to_not have_content(@dish2.calories)
+            expect(page).to have_content(@dish1.total_calories)
+            expect(page).to_not have_content(@dish2.total_calories)
           end
       end
     end
