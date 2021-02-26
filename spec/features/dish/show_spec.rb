21aa6de170cb6320abcbb963142f57f43ca50eb0
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-26 12:23:06 -0500

    Add tests for chef name and calorie count

--- spec/features/dish/show_spec.rb
+++ spec/features/dish/show_spec.rb
@@ -5,23 +5,35 @@ RSpec.describe "Dish Show Page" do
     set_up
   end
   describe "as a visitor" do
-    it "displays the dishes information" do
-      visit "/chefs/:chef_id/dishes/#{@dish_1.id}"
+    it "displays the dishes name and ingredients" do
+      visit "/chefs/#{@chef_a.id}/dishes/#{@dish_1.id}"
 
       expect(page).to have_content(@dish_1.name)
-      expect(page).to have_content(@dish_1.ingredients.pluck(:name))
+      expect(page).to have_content(@asparagus.name)
+      expect(page).to have_content(@snap_peas.name)
+      expect(page).to have_content(@noodles.name)
     end
-  end
+    it "displays the chef's name" do
+      visit "/chefs/#{@chef_a.id}/dishes/#{@dish_1.id}"
+
+      expect(page).to have_content(@chef_a.name)
+    end
+    it "displays the total calorie count" do
+      visit "/chefs/#{@chef_a.id}/dishes/#{@dish_1.id}"
+
+      expect(page).to have_content(@dish_1.calorie_count)
+    end
+   end
 
   def set_up
     @chef_a = Chef.create!(name: "Assa Paragus")
 
     @dish_1 = @chef_a.dishes.create!(name: "Asparagus Stirfy", description: "Sizzly!")
 
-    @asparagus = Ingredient.create!(name: "Asparagus")
-    @snap_peas = Ingredient.create!(name: "Snap Peas")
-    @noodles = Ingredient.create!(name: "Noodles")
-    @beef = Ingredient.create!(name: "Beef")
+    @asparagus = Ingredient.create!(name: "Asparagus", calories: 100)
+    @snap_peas = Ingredient.create!(name: "Snap Peas", calories: 100)
+    @noodles = Ingredient.create!(name: "Noodles", calories: 500)
+    @beef = Ingredient.create!(name: "Beef", calories: 500)
 
     @chef_a.dishes.find(@dish_1.id).ingredients << @asparagus
     @chef_a.dishes.find(@dish_1.id).ingredients << @snap_peas
