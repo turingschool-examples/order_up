0e54a220dd918d827333a5de577851a2199b4262
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-02-26 09:49:58 -0700

    Complete user story 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+@chef1 = Chef.create(name: 'Tommy', id: 1)
+@dish1 = Dish.create(name: 'Pizza', description: 'You might hate yourself after, but damn if it is not delicious', chef_id: 1)
+@ingredient1 = Ingredient.create(name: 'Sauce', calories: 50)
+@ingredient2 = Ingredient.create(name: 'Dough', calories: 200)
+@ingredient3 = Ingredient.create(name: 'Cheese', calories: 100)
+@ingredient4 = Ingredient.create(name: 'Pepperoni', calories: 150)
+DishIngredient.create(dish: @dish1, ingredient: @ingredient1)
+DishIngredient.create(dish: @dish1, ingredient: @ingredient2)
+DishIngredient.create(dish: @dish1, ingredient: @ingredient3)
+DishIngredient.create(dish: @dish1, ingredient: @ingredient4)
