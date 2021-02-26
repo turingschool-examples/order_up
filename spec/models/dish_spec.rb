2422dc86d53ba73a4f1fe77c60d69e6d10a8b7b4
Angel Breaux <abreaux@detaso.com>
2021-02-26 10:35:21 -0600

    Create spec test for total calories helper methods

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -10,4 +10,24 @@ RSpec.describe Dish, type: :model do
     it { should have_many :dish_ingredients }
     it { should have_many(:ingredients).through(:dish_ingredients) }
   end
+
+  before :each do
+    @chef = Chef.create!(name: "Bob")
+    @dish_1 = Dish.create!(name: "Pasta", description: 'Good dish', chef_id: @chef.id)
+    @dish_2 = Dish.create!(name: "Soup", description: 'Good soup', chef_id: @chef.id)
+
+    @ingredient_1 = @dish_1.ingredients.create!(name: 'onions', calories: 100)
+    @ingredient_2 = @dish_1.ingredients.create!(name: 'noodles', calories: 350)
+    @ingredient_3 = @dish_1.ingredients.create!(name: 'sauce', calories: 200)
+  end
+
+  describe 'instance methods' do
+    describe '#total_calories' do
+      it 'returns total calories from all ingredients' do
+        expected = @ingredient_1.calories + @ingredient_2.calories + @ingredient_3.calories
+        
+        expect(@dish_1.total_calories).to eq(expected)
+      end
+    end
+  end
 end
--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -5,4 +5,20 @@ RSpec.describe Ingredient, type: :model do
     it { should have_many :dish_ingredients }
     it { should have_many(:dishes).through(:dish_ingredients) }
   end
+
+  before :each do
+    @ingredient_1 = @dish_1.ingredients.create!(name: 'onions', calories: 100)
+    @ingredient_2 = @dish_1.ingredients.create!(name: 'noodles', calories: 350)
+    @ingredient_3 = @dish_1.ingredients.create!(name: 'sauce', calories: 200)
+  end
+
+  describe 'class methods' do
+    describe '::total_calories' do
+      it 'returns total calories' do
+        expected = @ingredient_1.calories + @ingredient_2.calories + @ingredient_3.calories
+
+        expect(Ingredient.total_calories).to eq(expected)
+      end
+    end
+  end
 end
