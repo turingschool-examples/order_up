dfb2d5a2a5b6b0d3965b0e186c9ec83dd39a5858
Jacob Volpe <volpe.jacob@gmail.com>
2021-02-26 10:55:38 -0700

    Finish story 1, pass tests, add seeds

--- spec/features/dish/show_spec.rb
+++ spec/features/dish/show_spec.rb
@@ -8,14 +8,14 @@ RSpec.describe "Dish Show page" do
 
     @ingredient1 = Ingredient.create!(name: "Fettucine", calories: 200)
     @ingredient2 = Ingredient.create!(name: "Chicken", calories: 250)
-    @ingredient3 = Ingredient.create!(name: "Chicken", calories: 350)
+    @ingredient3 = Ingredient.create!(name: "Alfredo", calories: 350)
 
     @dish_ing1 = DishIngredient.create!(dish_id: @dish.id, ingredient_id: @ingredient1.id)
     @dish_ing2 = DishIngredient.create!(dish_id: @dish.id, ingredient_id: @ingredient2.id)
     @dish_ing3 = DishIngredient.create!(dish_id: @dish.id, ingredient_id: @ingredient3.id)
   end
   describe "When I visit the dish show page" do 
-    it "it shows the dishs name and description" do 
+    it "it shows the dishs name and description, ingredients, chef name, total calories" do 
       visit "/dish/#{@dish.id}"
 
        expect(current_path).to eq("/dish/#{@dish.id}")
@@ -27,7 +27,21 @@ RSpec.describe "Dish Show page" do
      within("#description") do 
           expect(page).to have_content("Dish Description: #{@dish.description}")
      end
+
+     within("#ingredients") do 
+      expect(page).to have_content("Ingredients")
+      expect(page).to have_content("#{@ingredient1.name}")
+      expect(page).to have_content("#{@ingredient2.name}")
+      expect(page).to have_content("#{@ingredient3.name}")
+     end
+
+     within("#chef-name") do 
+      expect(page).to have_content("Chef Name: #{@chef.name}")
+     end
      
+      within("#total-calories") do 
+        expect(page).to have_content("Total Calories: 800")
+      end
 
     end
   end
--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -9,4 +9,23 @@ RSpec.describe Dish, type: :model do
     it {should have_many :dish_ingredients}
     it {should have_many(:ingredients).through(:dish_ingredients)}
   end
+  describe 'instance methods' do 
+    it 'returns total calories of dish' do 
+      chef = Chef.create!(name: "Bourdain")
+
+      dish = Dish.create!(name: "Pasta", description: "Chicken Alfredo", chef_id: chef.id)
+
+      ingredient1 = Ingredient.create!(name: "Fettucine", calories: 200)
+      ingredient2 = Ingredient.create!(name: "Chicken", calories: 250)
+      ingredient3 = Ingredient.create!(name: "Alfredo", calories: 350)
+
+      dish_ing1 = DishIngredient.create!(dish_id: dish.id, ingredient_id: ingredient1.id)
+      dish_ing2 = DishIngredient.create!(dish_id: dish.id, ingredient_id: ingredient2.id)
+      dish_ing3 = DishIngredient.create!(dish_id: dish.id, ingredient_id: ingredient3.id)
+
+      total_cals = dish.total_calories
+      
+      expect(total_cals).to eq(800)
+    end
+  end
 end
