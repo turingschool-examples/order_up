42307d62b6b462c8aa7972eab756fecb42eaf35f
Andrew Johnston <av.johnst@gmail.com>
2021-02-26 11:31:55 -0700

    Add new route for dish ingredient destroy

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get "/dishes/:id", to: "dishes#show"
   get "/chefs/:id", to: "chefs#show"
   get "/chefs/:id/ingredients", to: "ingredients#index"
-  patch "/dishes/:id", to: "dishes#update"
+  # patch "/dishes/:id", to: "dishes#update"
+  delete "/dishes/:id/ingredients", to: "dish_ingredients#destroy"
 end
