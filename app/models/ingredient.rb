1a5f117c9ad8e085257ae0726ed97ee95ab8c4fd
Jenny Branham <branhamjenny@gmail.com>
2021-02-26 11:32:00 -0600

    Add relationships to models

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,7 @@
 class Ingredient < ApplicationRecord
   has_many :dish_ingredients
   has_many :dishes, through: :dish_ingredients
+  has_many :chefs, through: :dishes
+
   validates_presence_of :name
 end
--- spec/models/dish_ingredient_spec.rb
+++ spec/models/dish_ingredient_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe DishIngredient, type: :model do
   describe "relationships" do
     it { should belong_to :dish }
     it { should belong_to :ingredient }
+    it {should have_one(:chef).through(:dish)}
   end
 end
