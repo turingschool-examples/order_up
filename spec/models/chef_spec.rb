b0f7c707d1416bb3f3a027cdce76e8e941070430
Kris Litman <duguy8@gmail.com>
2021-02-26 12:46:35 -0500

    Added model test to chef for most_popular_ingredients. Working on sad paths

--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -58,5 +58,55 @@ RSpec.describe Chef, type: :model do
       expect(expected[0].name).to eq(@puff_pastry.name)
       expect(expected[1].name).to eq(@noodles.name)
     end
+    it "#most_popular_ingredients" do
+        @gordon = Chef.create!(
+          name: "Gordon Ramsey"
+        )
+        @penne = @gordon.dishes.create!(
+          name: "Penne Vodka",
+          description: "Saucy"
+        )
+        @wellington = @gordon.dishes.create!(
+          name: "Beef Wellington",
+          description: "Bloody Brilliant"
+        )
+        @risotto = @gordon.dishes.create!(
+          name: "Seafood Risotto",
+          description: "Also Bloody Brilliant"
+        )
+        @beef = Ingredient.create!(
+          name: "Dont forget the lamb sauce",
+          calories: 1000
+        )
+        @puff_pastry = Ingredient.create!(
+          name: "Puff Pastry",
+          calories: 800
+        )
+        @noodles = Ingredient.create!(
+          name: "Penne",
+          calories: 300
+        )
+        @sauce = Ingredient.create!(
+          name: "Sauce",
+          calories: 800
+        )
+        @parm = Ingredient.create!(
+          name: "Cheese",
+          calories: 400
+        )
+        @salmon = Ingredient.create!(
+          name: "Salmon",
+          calories: 500
+        )
+        @wellington.ingredients << [@beef, @puff_pastry]
+        @penne.ingredients << [@beef, @noodles]
+        @risotto.ingredients << [@beef, @noodles]
+
+        expected = @gordon.most_popular_ingredients
+
+        expect(expected[2].name).to eq(@puff_pastry.name)
+        expect(expected[0].name).to eq(@beef.name)
+        expect(expected[1].name).to eq(@noodles.name)
+    end
   end
 end
