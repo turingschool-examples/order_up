4f62d7720fbf52bebe349a38f215daf714b8342f
Angel Breaux <abreaux@detaso.com>
2021-02-26 11:58:46 -0600

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+@chef = Chef.create!(name: "Bob")
+@dish_1 = Dish.create!(name: "Pasta", description: 'Good dish', chef_id: @chef.id)
+@dish_2 = Dish.create!(name: "Soup", description: 'Good soup', chef_id: @chef.id)
+
+@ingredient_1 = @dish_1.ingredients.create!(name: 'onions', calories: 100)
+@ingredient_2 = @dish_1.ingredients.create!(name: 'noodles', calories: 350)
+@ingredient_3 = @dish_1.ingredients.create!(name: 'sauce', calories: 200)
+
+@ingredient_4 = @dish_2.ingredients.create!(name: 'corn', calories: 50)
+@ingredient_5 = @dish_2.ingredients.create!(name: 'chicken stock', calories: 100)
+@ingredient_6 = @dish_2.ingredients.create!(name: 'bell peppers', calories: 25)
