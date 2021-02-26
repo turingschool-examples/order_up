7af232fe0841ddb327e414b6192fe83f8e4bc75e
Angel Breaux <abreaux@detaso.com>
2021-02-26 11:12:25 -0600

    Update ingredient spec to test a find from chef method

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -7,9 +7,18 @@ RSpec.describe Ingredient, type: :model do
   end
 
   before :each do
+    @chef = Chef.create!(name: "Bob")
+
+    @dish_1 = Dish.create!(name: "Pasta", description: 'Good dish', chef_id: @chef.id)
+    @dish_2 = Dish.create!(name: "Soup", description: 'Good soup', chef_id: @chef.id)
+
     @ingredient_1 = Ingredient.create!(name: 'onions', calories: 100)
     @ingredient_2 = Ingredient.create!(name: 'noodles', calories: 350)
     @ingredient_3 = Ingredient.create!(name: 'sauce', calories: 200)
+
+    DishIngredient.create!(dish: @dish_1, ingredient: @ingredient_1)
+    DishIngredient.create!(dish: @dish_1, ingredient: @ingredient_2)
+    DishIngredient.create!(dish: @dish_1, ingredient: @ingredient_3)
   end
 
   describe 'class methods' do
@@ -18,6 +27,14 @@ RSpec.describe Ingredient, type: :model do
         expected = @ingredient_1.calories + @ingredient_2.calories + @ingredient_3.calories
 
         expect(Ingredient.total_calories).to eq(expected)
+      end
+    end
+
+    describe '::find_from_chef' do
+      it 'returns ingredients from dishes that belong to a specific chef' do
+        expected = [@ingredient_1, @ingredient_2, @ingredient_3]
+
+        expect(Ingredient.find_from_chef(@chef.id)).to eq(expected)
       end
     end
   end
