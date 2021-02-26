5ddede1f8d63a3b960d38898189797f0062fcb17
Trevor Robinson <trevor.robi1254@gmail.com>
2021-02-26 10:18:34 -0700

    Complete User Story 1

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -7,5 +7,17 @@ RSpec.describe Dish, type: :model do
   end
   describe "relationships" do
     it {should belong_to :chef}
+    it {should have_many :dish_ingredients}
+    it {should have_many(:ingredients).through(:dish_ingredients)}
+  end
+
+  describe "instance methods" do
+    it "should calculate total callories of ingredients in the dish" do
+      @chef = Chef.create(name: "Swedish Chef")
+      @dish = @chef.dishes.create(name: "Orgabork", description: "Bork Bork Bork")
+      @ingredient1 = @dish.ingredients.create(name: "Chicken", calories: 50)
+      @ingredient2 = @dish.ingredients.create(name: "Fish", calories: 50)
+      expect(@dish.total_callories).to eq(100)
+    end
   end
 end
