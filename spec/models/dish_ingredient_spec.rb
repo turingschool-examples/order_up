6b0e94f3ed313d69d521a63747d0a049b3b334a7
Trevor Suter <Trevorsuter@icloud.com>
2021-02-26 09:22:00 -0700

    validations

--- spec/models/dish_ingredient_spec.rb
+++ spec/models/dish_ingredient_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe DishIngredient, type: :model do
     it {should belong_to :dish}
     it {should belong_to :ingredient}
   end
+  describe "validations" do
+    it {should validate_presence_of :dish}
+    it {should validate_presence_of :ingredient}
+  end
 end
--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe Ingredient, type: :model do
     it {should have_many :dish_ingredients}
     it {should have_many(:dishes).through(:dish_ingredients)}
   end
+  describe "validations" do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :calories}
+  end
 end
