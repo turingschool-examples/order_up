4743d0a3a2695bea3aeaf351b41f0d7e83eee1e3
Andrew Johnston <av.johnst@gmail.com>
2021-02-26 11:18:28 -0700

    Finish tests for extension

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -19,14 +19,42 @@ RSpec.describe 'Chef show page' do
       click_link("Chef's Ingredients")
       expect(current_path).to eq("/chefs/#{@jon.id}/ingredients")
     end
+
+    it 'show page has the chefs most popular dishes' do
+      visit "/chefs/#{@jon.id}"
+
+      expect(page).to have_content("Chef's Three Most Popular Dishes:")
+
+      within ".ingredient-#{@potatoes.id}" do
+        expect(page).to have_content(@potatoes.name)
+      end
+
+      within ".ingredient-#{@pork.id}" do
+        expect(page).to have_content(@pork.name)
+      end
+
+      within ".ingredient-#{@salmon.id}" do
+        expect(page).to have_content(@salmon.name)
+      end
+
+    end
   end
 
   def set_up
     @jon = Chef.create!(name: "Jon Favreau")
     @fish = @jon.dishes.create!(name: "Fishy Dinner", description: "Delicious food")
+    @cuban = @jon.dishes.create!(name: "Cuban Sandwich", description: "My specialty")
+    @tacos = @jon.dishes.create!(name: "Street Tacos", description: "A taste of mexico")
     @salmon = Ingredient.create!(name: "Salmon", calories: 250)
     @potatoes = Ingredient.create!(name: "Potatoes", calories: 500)
+    @bread = Ingredient.create!(name: "Bread", calories: 300)
+    @pork = Ingredient.create!(name: "Pork", calories: 225)
     DishIngredient.create!(dish: @fish, ingredient: @salmon)
     DishIngredient.create!(dish: @fish, ingredient: @potatoes)
+    DishIngredient.create!(dish: @cuban, ingredient: @potatoes)
+    DishIngredient.create!(dish: @cuban, ingredient: @bread)
+    DishIngredient.create!(dish: @cuban, ingredient: @pork)
+    DishIngredient.create!(dish: @tacos, ingredient: @pork)
+    DishIngredient.create!(dish: @tacos, ingredient: @potatoes)
   end
 end
--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -13,22 +13,45 @@ RSpec.describe Chef, type: :model do
 
   describe 'instance methods' do
     it 'chef ingredients' do
-      expect(@jon.chef_ingredients).to eq([@salmon, @potatoes])
+      expected = [@salmon, @potatoes, @bread, @pork]
+
+      expect(@jon.chef_ingredients).to eq(expected)
     end
 
     it 'chef ingredient dishes' do
-      expected = [@salmon.id, @potatoes.id]
+      expected = [@salmon.id, @potatoes.id, @bread.id, @pork.id]
 
       expect(@jon.chef_ingredient_dishes).to eq(expected)
+    end
+
+    it 'most popular ingredients id' do
+      expected = [@potatoes.id, @pork.id, @salmon.id]
+
+      expect(@jon.most_popular_ingredients_id).to eq(expected)
+    end
+
+    it 'most popular ingredients' do
+      expected = [@potatoes, @pork, @salmon]
+
+      expect(@jon.most_popular_ingredients).to eq(expected)
     end
   end
 
   def set_up
     @jon = Chef.create!(name: "Jon Favreau")
     @fish = @jon.dishes.create!(name: "Fishy Dinner", description: "Delicious food")
+    @cuban = @jon.dishes.create!(name: "Cuban Sandwich", description: "My specialty")
+    @tacos = @jon.dishes.create!(name: "Street Tacos", description: "A taste of mexico")
     @salmon = Ingredient.create!(name: "Salmon", calories: 250)
     @potatoes = Ingredient.create!(name: "Potatoes", calories: 500)
+    @bread = Ingredient.create!(name: "Bread", calories: 300)
+    @pork = Ingredient.create!(name: "Pork", calories: 225)
     DishIngredient.create!(dish: @fish, ingredient: @salmon)
     DishIngredient.create!(dish: @fish, ingredient: @potatoes)
+    DishIngredient.create!(dish: @cuban, ingredient: @potatoes)
+    DishIngredient.create!(dish: @cuban, ingredient: @bread)
+    DishIngredient.create!(dish: @cuban, ingredient: @pork)
+    DishIngredient.create!(dish: @tacos, ingredient: @pork)
+    DishIngredient.create!(dish: @tacos, ingredient: @potatoes)
   end
 end
