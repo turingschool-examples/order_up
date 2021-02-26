441c9e08e6d8d3905d1023e6b6ffc5699d594d44
Kris Litman <duguy8@gmail.com>
2021-02-26 12:44:07 -0500

    Extension complete

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -3,6 +3,18 @@ class Chef < ApplicationRecord
   has_many :dishes
 
   def find_unique_ingredients
-    dishes.joins(:ingredients).select('ingredients.*').distinct
+    dishes
+    .joins(:ingredients)
+    .select('ingredients.*')
+    .distinct
+  end
+
+  def most_popular_ingredients
+    dishes
+    .joins(:ingredients)
+    .select('ingredients.name, count(*) AS ingredient_count')
+    .group('ingredients.id')
+    .order('ingredient_count DESC')
+    .limit(3)
   end
 end
