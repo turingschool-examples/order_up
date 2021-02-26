e059a102538c8e93cc10a51d7aec138839e46034
Angel Breaux <abreaux@detaso.com>
2021-02-26 12:47:20 -0600

    Create AR to find popular ingredients for chef id

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -9,4 +9,15 @@ class Ingredient < ApplicationRecord
   def self.find_from_chef(chef_id)
     joins(:dishes).where('dishes.chef_id = ?', chef_id)
   end
+
+  def self.find_distinct(chef_id)
+    find_from_chef(chef_id).distinct
+  end
+
+  def self.popular_ingredients_for(chef_id)
+    find_from_chef(chef_id)
+    .group(:id)
+    .having('count(ingredients.id) > 1')
+    .limit(3)
+  end
 end
