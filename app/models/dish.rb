fd6f43cba588d4ad7cb1ae73b6286f30966e9c78
Jacob Arellano <jmm.arellano@gmail.com>
2021-02-26 10:16:49 -0700

    Add specs and code for user story 1

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -3,4 +3,12 @@ class Dish <ApplicationRecord
   belongs_to :chef
   has_many :dish_ingredients
   has_many :ingredients, through: :dish_ingredients
+
+  def chef_name
+    chef.name
+  end
+
+  def calories
+    ingredients.sum(:calories)
+  end
 end
--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -1,6 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Dish, type: :model do
+  before(:each) do
+    @chef = Chef.create!(name: "Big Sloppy Sal")
+    @dish_1 = @chef.dishes.create!(name: "Pizza", description: "Better than your mothers!")
+
+    @sauce = Ingredient.create!(name: "tomatoe sauce", calories: 50)
+    @cheese = Ingredient.create!(name: "cheese", calories: 150)
+    @dough = Ingredient.create!(name: "pizza dough", calories: 100)
+
+    DishIngredient.create!(dish: @dish_1, ingredient: @sauce)
+    DishIngredient.create!(dish: @dish_1, ingredient: @cheese)
+    DishIngredient.create!(dish: @dish_1, ingredient: @dough)
+  end
+
   describe "validations" do
     it {should validate_presence_of :name}
     it {should validate_presence_of :description}
@@ -9,5 +22,19 @@ RSpec.describe Dish, type: :model do
     it {should belong_to :chef}
     it {should have_many :dish_ingredients}
     it {should have_many(:ingredients).through(:dish_ingredients) }
+  end
+
+  describe "instance methods" do
+    describe '#chef_name' do
+      it 'Create a test for a corresponding method that returns an chef name' do
+        expect(@dish_1.chef_name).to eq("Big Sloppy Sal")
+      end
+    end
+    
+    describe '#calories' do
+      it 'Create a test for a corresponding method that returns a dishes calories' do
+        expect(@dish_1.calories).to eq(300)
+      end
+    end
   end
 end
