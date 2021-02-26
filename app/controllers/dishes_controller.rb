d4337d4696c53715ea5c6a906690306199ef38d7
Andrew Johnston <av.johnst@gmail.com>
2021-02-26 10:30:20 -0700

    Finish user story 1-3

---
+++ app/controllers/dishes_controller.rb
@@ -0,0 +1,17 @@
+class DishesController < ApplicationController
+  def show
+    @dish = Dish.find(params[:id])
+    @ingredients = @dish.ingredients
+    @chef = @dish.chef
+  end
+
+  def update
+    if params[:remove]
+      @dish = Dish.find(params[:id])
+      @dish_ingredient = DishIngredient.where(dish_id: @dish.id, ingredient_id: params[:remove]).first
+      DishIngredient.find(@dish_ingredient.id).destroy
+
+      redirect_to "/dishes/#{@dish.id}"
+    end
+  end
+end
--- app/models/chef.rb
+++ app/models/chef.rb
@@ -2,4 +2,8 @@ class Chef <ApplicationRecord
   validates_presence_of :name
   has_many :dishes
 
+  def chef_ingredients
+    di = DishIngredient.where(dish_id: self.dishes.pluck(:id)).pluck(:ingredient_id)
+    Ingredient.find(di)
+  end
 end
