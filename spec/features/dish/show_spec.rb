7014293e7bcf41a9923833a6a04819e02a7155c5
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-26 12:01:06 -0500

    Add test for user story 1

---
+++ spec/features/dish/show_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "Dish Show Page" do
+  before :each do
+    set_up
+  end
+  describe "as a visitor" do
+    it "displays the dishes information" do
+      visit "/chefs/:chef_id/dishes/:id"
+      
+      expect(page).to have_content(@dish_1.name)
+      expect(page).to have_content(@dish_1.ingredients.pluck(:name))
+    end
+  end
+
+  def set_up
+    @chef_a = Chef.create!(name: "Assa Paragus")
+
+    @dish_1 = @chef_a.dishes.create!(name: "Asparagus Stirfy", description: "Sizzly!")
+
+    @asparagus = Ingredient.create!(name: "Asparagus")
+    @snap_peas = Ingredient.create!(name: "Snap Peas")
+    @noodles = Ingredient.create!(name: "Noodles")
+    @beef = Ingredient.create!(name: "Beef")
+
+    @chef_a.dishes.find(@dish_1.id).ingredients << @asparagus
+    @chef_a.dishes.find(@dish_1.id).ingredients << @snap_peas
+    @chef_a.dishes.find(@dish_1.id).ingredients << @noodles
+  end
+end
