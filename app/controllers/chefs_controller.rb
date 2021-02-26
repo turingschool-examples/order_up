8af10bf816627efedbbc644385d9e7d2205fdda0
Angel Breaux <abreaux@detaso.com>
2021-02-26 12:47:59 -0600

    Create variables to find informatin to show on show page

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,5 +1,6 @@
 class ChefsController < ApplicationController
   def show
     @chef = Chef.find(params[:id])
+    @popular_ingredients = Ingredient.popular_ingredients_for(@chef.id)
   end
 end
--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,5 +1,5 @@
 class IngredientsController < ApplicationController
   def index
-    @ingredients = Ingredient.find_from_chef(params[:chef_id])
+    @ingredients = Ingredient.find_distinct(params[:chef_id])
   end
 end
