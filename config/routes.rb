ef05ac983669480b75bf1207c71e7e03f5abeced
Angel Breaux <abreaux@detaso.com>
2021-02-26 10:56:59 -0600

    Create new route for ingredients index through a chef

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :chefs, only: :show
+  resources :chefs, only: :show do
+    resources :ingredients, only: :index
+  end
   resources :dishes, only: [:show, :destroy]
 end
