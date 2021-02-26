9655c4420bc78ce0e807ab49695a5b283b26f5de
Jacob Volpe <volpe.jacob@gmail.com>
2021-02-26 11:54:46 -0700

    Embarrassed for myself

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :dish, only: [:show]
+  resources :dish, only: [:show] do 
+    resources :ingredient, only: [:destroy]
+  end
 
   resources :chef, only: [:show] do 
     resources :ingredient, only: [:index] 
