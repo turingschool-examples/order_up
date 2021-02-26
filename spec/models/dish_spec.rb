030736a356886ea4a114d08d5cfc79fff523c946
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-26 12:28:52 -0500

    Create test for calorie_count. Create passing method

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Dish, type: :model do
+  before :each do
+    set_up
+  end
   describe "validations" do
     it { should validate_presence_of :name }
     it { should validate_presence_of :description }
@@ -8,5 +11,27 @@ RSpec.describe Dish, type: :model do
   describe "relationships" do
     it { should belong_to :chef}
     it { should have_many(:ingredients).through(:dish_ingredients) }
+  end
+
+  describe "instance methods" do
+    describe "#calorie_count" do
+      it "totals the calories in a dish based on the ingredients" do
+        expect(@dish_1.calorie_count).to eq(700)
+      end
+    end
+  end
+  def set_up
+    @chef_a = Chef.create!(name: "Assa Paragus")
+
+    @dish_1 = @chef_a.dishes.create!(name: "Asparagus Stirfy", description: "Sizzly!")
+
+    @asparagus = Ingredient.create!(name: "Asparagus", calories: 100)
+    @snap_peas = Ingredient.create!(name: "Snap Peas", calories: 100)
+    @noodles = Ingredient.create!(name: "Noodles", calories: 500)
+    @beef = Ingredient.create!(name: "Beef", calories: 500)
+
+    @chef_a.dishes.find(@dish_1.id).ingredients << @asparagus
+    @chef_a.dishes.find(@dish_1.id).ingredients << @snap_peas
+    @chef_a.dishes.find(@dish_1.id).ingredients << @noodles
   end
 end
