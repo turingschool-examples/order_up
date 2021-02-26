220f3c5765772eb0cbd7403b1e74de092d712ab7
Alex Schwartz <aschwartz@detaso.com>
2021-02-26 11:40:50 -0600

    Add basic chefs show page

---
+++ spec/features/chefs/show_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe 'Chef show page' do
+  before :each do
+    @guy = Chef.create!(name: 'Guy Fieri')
+    create_burger_for_guy
+    create_nachos_for_guy
+  end
+
+  describe 'as a visitor' do
+    it 'shows name of the chef' do
+      visit chef_path(@guy)
+
+      within('#chef-info') do
+        expect(page).to have_content('Guy')
+      end
+    end
+  end
+
+  def create_burger_for_guy
+    @burger = @guy.dishes.create!(name: 'Burger', description: 'A classic.')
+    @burger.ingredients.create!(name: 'Bun', calories: 10)
+    @burger.ingredients.create!(name: 'Meat', calories: 50)
+  end
+
+  def create_nachos_for_guy
+    @nachos = @guy.dishes.create!(name: 'Nachos', description: "You can't go wrong with this.")
+    @nachos.ingredients.create!(name: 'Chip-os', calories: 10)
+    @nachos.ingredients.create!(name: 'Cheese-os', calories: 60)
+    @nachos.ingredients.create!(name: 'Jalepenos', calories: 5)
+  end
+end
