4e8ab07306f5d558fa51c851dde9a916a1347afb
Dominic Padula <domopadula2192@gmail.com>
2021-02-26 10:45:54 -0700

    has to create another association for chefs to ingredients all code passing for iteration 2

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :chefs do
     resources :dishes, only: [:show]
-  end 
+    resources :ingredients, controller: 'chef_ingredients', only: [:index]
+  end
 end
