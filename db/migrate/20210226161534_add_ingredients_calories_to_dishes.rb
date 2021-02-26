9cde8d6efa50c838e1641eca38e8005983b073e8
kylejschulz <kylschulz@gmail.com>
2021-02-26 11:52:23 -0500

    add dish ingredient spec

--- db/migrate/20210226161534_add_ingredients_calories_to_dishes.rb
+++
@@ -1,6 +0,0 @@
-class AddIngredientsCaloriesToDishes < ActiveRecord::Migration[5.2]
-  def change
-    add_column :dishes, :ingredients, :string
-    add_column :dishes, :calories, :integer
-  end
-end
