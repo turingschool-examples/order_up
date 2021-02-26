508c00a92e83568bd61c141cab422402421ab259
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-26 12:23:51 -0500

    Add instance variables for chef and ingredients

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -2,5 +2,7 @@ class DishesController < ApplicationController
 
   def show
     @dish = Dish.find(params[:id])
-  end 
+    @dish_ingredients = @dish.ingredients
+    @dish_chef = @dish.chef.name
+  end
 end
