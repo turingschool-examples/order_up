ad4da2ee7e9ad9e600b24b97bafc18004500eb11
Arika Turing <ablenker@gmail.com>
2021-02-26 11:03:29 -0700

    Add test and passing code for chefs ingredient index

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -2,4 +2,10 @@ class Chef < ApplicationRecord
   validates_presence_of :name
   has_many :dishes
 
+  def unique_ingredients
+    dishes.joins(:ingredients)
+    .select('dishes.*, ingredients.*')
+    .distinct
+    .pluck('ingredients.name')
+  end
 end
--- spec/features/chef/show_spec.rb
+++ spec/features/chef/show_spec.rb
@@ -3,10 +3,23 @@ require 'rails_helper'
 RSpec.describe 'Chef show page', type: :feature do
   before :each do
     @chef = Chef.create!(name: "Billy bob")
+
     @dish_1 = @chef.dishes.create!(name: "Salad", description: "Super healthy")
-    @ingredient_1 = @dish_1.ingredients.create!(name: "Spinach", calories: 25)
-    @ingredient_2 = @dish_1.ingredients.create!(name: "Feta cheese", calories: 50)
-    @ingredient_3 = @dish_1.ingredients.create!(name: "Tomatoes", calories: 15)
+    @dish_2 = @chef.dishes.create!(name: "Spaghetti", description: "Rich and filling")
+
+    @ingredient_1 = Ingredient.create!(name: "Spinach", calories: 25)
+    @ingredient_2 = Ingredient.create!(name: "Feta cheese", calories: 50)
+    @ingredient_3 = Ingredient.create!(name: "Tomatoes", calories: 15)
+    @ingredient_4 = Ingredient.create!(name: "Pasta", calories: 75)
+    @ingredient_5 = Ingredient.create!(name: "Mozzarella", calories: 30)
+
+    @d1_i1 = DishIngredient.create!(dish_id: @dish_1.id, ingredient_id: @ingredient_1.id)
+    @d1_i2 = DishIngredient.create!(dish_id: @dish_1.id, ingredient_id: @ingredient_2.id)
+    @d1_i3 = DishIngredient.create!(dish_id: @dish_1.id, ingredient_id: @ingredient_3.id)
+
+    @d2_i3 = DishIngredient.create!(dish_id: @dish_2.id, ingredient_id: @ingredient_3.id)
+    @d2_i4 = DishIngredient.create!(dish_id: @dish_2.id, ingredient_id: @ingredient_4.id)
+    @d2_i5 = DishIngredient.create!(dish_id: @dish_2.id, ingredient_id: @ingredient_5.id)
   end
 
   it 'Displays chefs name and a link to their ingredients' do
@@ -16,4 +29,19 @@ RSpec.describe 'Chef show page', type: :feature do
     expect(page).to have_link("Ingredents they use")
   end
 
+  it 'Links takes you to unique list of chefs ingredients' do
+    visit "/chefs/#{@chef.id}"
+
+    click_on "Ingredents they use"
+
+    expect(current_path).to eq("/chefs/#{@chef.id}/ingredients")
+
+    within("#ingredients") do
+      expect(page).to have_content(@ingredient_1.name, count: 1)
+      expect(page).to have_content(@ingredient_2.name)
+      expect(page).to have_content(@ingredient_3.name)
+      expect(page).to have_content(@ingredient_4.name)
+      expect(page).to have_content(@ingredient_5.name)
+    end
+  end
 end
