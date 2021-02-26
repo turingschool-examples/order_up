0b90f94ab4e221af0b76d26e39b4dd193755442e
Dominic Padula <domopadula2192@gmail.com>
2021-02-26 09:51:12 -0700

    write feature tests and passing code up to instance method for dishes

---
+++ app/controllers/dishes_controller.rb
@@ -0,0 +1,7 @@
+class DishesController < ApplicationController
+
+  def show
+    @chef = Chef.find(params[:chef_id])
+    @dish = Dish.find(params[:id])
+  end
+end
---
+++ spec/features/dish/show_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'dish show page' do
+    before(:each) do
+      @chef = Chef.create(name: "Bobby Flay")
+      @dish_1 = @chef.dishes.create(name: "Cheese Burger", description: "Delicious")
+      @cheese = @dish_1.ingredients.create(name: "American cheese", calories: 100)
+      @beef = @dish_1.ingredients.create(name: "beef", calories: 200)
+      DishIngredient.create(dish: @dish_1, ingredient: @cheese)
+      DishIngredient.create(dish: @dish_1, ingredient: @beef)
+    end
+
+    it "can show dish name and description" do
+      visit chef_dish_path(@chef, @dish_1)
+      expect(page).to have_content(@dish_1.name)
+      expect(page).to have_content(@chef.name)
+      expect(page).to have_content(@cheese.name)
+      expect(page).to have_content(@beef.name)
+      expect(page).to have_content("300 calories")
+  end
+end
